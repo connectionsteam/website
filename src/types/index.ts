91cddb7a0d3318be55b2e468943ce3abacaa7855
Spyei <caio.salamao1234@gmail.com>
2024-06-06 21:57:08 -0300

    feat: new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,19 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
 
 export interface UserStructure {
     id: string;
     username: string;
     avatar: string;
+}
+
+export type LanguageType = "pt-BR" | "en-US";
+
+export interface UserContextProps {
+    user: UserStructure | null;
+    setUser: (user: UserStructure | null) => void;
+}
+
+export interface LanguageContextProps {
+    language: LanguageType;
+    setLanguage: Dispatch<SetStateAction<LanguageType>>;
 }
