04dd300e0d7cfb9b8257e45859ec591562982238
Spyei <caio.salamao1234@gmail.com>
2024-06-07 16:05:25 -0300

    feat: nextui providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,16 +2,19 @@
 import { IsClientProvider } from "@/contexts/Client";
 import { LanguageProvider } from "@/contexts/Language";
 import { UserProvider } from "@/contexts/User";
+import { NextUIProvider } from "@nextui-org/react";
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
     return (
-        <UserProvider>
-            <LanguageProvider>
-                <IsClientProvider>
-                    {children}
-                </IsClientProvider>
-            </LanguageProvider>
-        </UserProvider>
+        <NextUIProvider>
+            <UserProvider>
+                <LanguageProvider>
+                    <IsClientProvider>
+                        {children}
+                    </IsClientProvider>
+                </LanguageProvider>
+            </UserProvider>
+        </NextUIProvider>
     );
 }
