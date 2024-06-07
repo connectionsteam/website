3aaea92145f09a3c798ba6d91e47e1aa93406950
Spyei <caio.salamao1234@gmail.com>
2024-06-07 16:21:16 -0300

    fix: same font in all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import "../app/globals.css"
+import "../app/globals.css";
 import Header from "@/components/Header";
 import Providers from "@/providers";
 import { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <section className="bg-neutral-900">
+        <section className={`${inter.className} bg-neutral-900 min-h-screen`}>
             <Providers>
                 <Header />
                 <Component {...pageProps} />
