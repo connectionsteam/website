6c08f463d55ebe9694a58bde22dc1441bab72257
Spyei <caio.salamao1234@gmail.com>
2024-06-07 16:21:50 -0300

    chore: page component folder

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Page from "@/components/Page";
-
-export default function Home() {
-    return <Page/>
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Page from "@/components/Page";
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Connections | Home</title>
+            </Head>
+            <Page />
+        </>
+    );
+}
