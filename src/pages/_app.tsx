9ff8ccc5610344b8f812e8e78f6be4db40731474
Spyei <caio.salamao1234@gmail.com>
2024-06-06 21:58:31 -0300

    feat: providers component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
+import "../app/globals.css"
 import Header from "@/components/Header";
+import Providers from "@/providers";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
-            <Header/>
-            <Component {...pageProps} />
-        </>
+        <section className="bg-neutral-900">
+            <Providers>
+                <Header />
+                <Component {...pageProps} />
+            </Providers>
+        </section>
     );
 }
