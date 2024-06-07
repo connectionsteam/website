765fbadf6232d3ba43d8e0a2b50d1a2d30a3227d
Spyei <caio.salamao1234@gmail.com>
2024-06-06 21:58:38 -0300

    feat: locales

---
+++ src/locale/index.ts
@@ -0,0 +1,12 @@
+export const languages = {
+    "pt-BR": {
+        home: {
+            description: "Connections é um bot para conexões entre servidores, ele foi pensada para movimentação do seu servidor ajudando com novos usuários e novas amizades!",
+        }
+    },
+    "en-US": {
+        "home": {
+            description: "Connections is a bot for connecting servers, it was designed to help move your server helping new users and new friends!",
+        }
+    }
+}
