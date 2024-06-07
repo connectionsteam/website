1535d15cdd8cfdd5bfadac10455d6aedd4d0ec60
Spyei <caio.salamao1234@gmail.com>
2024-06-06 21:58:01 -0300

    feat: underline component

---
+++ src/components/Mixed/Underline.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function Underline({ children }: { children: ReactNode }) {
+    return (
+        <span className="bg-left-bottom bg-gradient-to-r from-fuchsia-500 via-purple-500 to-indigo-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"> 
+            {children}
+        </span>
+    )
+}
