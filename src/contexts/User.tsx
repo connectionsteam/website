2304e169e6b66ba378c89b35a0509a79bbf0002d
Spyei <caio.salamao1234@gmail.com>
2024-06-08 11:58:10 -0300

    feat: improved user context

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -1,6 +1,8 @@
 "use client"
-import React, { createContext, useState, useEffect, PropsWithChildren, ReactNode } from "react";
+import React, { createContext, useState, useEffect, ReactNode } from "react";
 import { UserContextProps, UserStructure } from "@/types";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 export const UserContext = createContext<UserContextProps>({
     user: null,
@@ -11,11 +13,22 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     const [user, setUser] = useState<UserStructure | null>(null);
 
     useEffect(() => {
-        setUser({
-            avatar: "4007e7943493138d10aeb5d6e64e481c",
-            id: "955095844275781693",
-            username: "Spyei"
-        });
+        const fetchUser = async () => {
+            const res = await axios.get("/api/auth/user", { 
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `${Cookies.get("discordUser")}`
+                }
+            });
+
+            const data = await res.data;
+
+            setUser(data);
+        };
+
+        if (Cookies.get("discord_user")) {
+            fetchUser();
+        }
     }, [])
 
     return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
