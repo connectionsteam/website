996f84a17ffccfe12f4b0ebcdbf8b3124736f17e
Spyei <caio.salamao1234@gmail.com>
2024-06-08 12:45:21 -0300

    feat: avatar component

--- src/components/Header/User.tsx
+++ src/components/Header/User.tsx
@@ -1,5 +1,4 @@
 import { UserContext } from "@/contexts/User";
-import { Avatar } from "@nextui-org/avatar";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
 import { useContext } from "react";
 import { LuLink, LuLogOut, LuServer } from "react-icons/lu";
@@ -7,6 +6,7 @@ import Cookies from "js-cookie";
 import { useIsClient } from "@/contexts/Client";
 import axios from "axios";
 import Link from "next/link";
+import Avatar from "../Mixed/Avatar";
 
 const url = "https://discord.com/oauth2/authorize?client_id=1021810246462738432&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback&scope=identify+guilds";
 
@@ -37,14 +37,7 @@ export default function AuthUser({ type }: { type: "mobile" | "desktop" }) {
                 <DropdownTrigger className="focus:bg-neutral-900 mb-4">
                     <button className="flex gap-2 justify-center items-center transition p-3 hover:bg-neutral-900 border-neutral-800 border-2 rounded-lg w-full">
                         <span className="font-bold flex flex-grow">{user.username}</span>
-                        <Avatar
-                            src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
-                            classNames={{
-                                base: "bg-gradient-to-r from-fuchsia-500 via-purple-500 to-indigo-500",
-                                icon: "text-black/80",
-                            }}
-                            showFallback
-                        />
+                        <Avatar src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} />
                     </button>
                 </DropdownTrigger>
                 <DropdownMenu className="bg-neutral-800 rounded-lg">
@@ -62,7 +55,7 @@ export default function AuthUser({ type }: { type: "mobile" | "desktop" }) {
                     </DropdownItem>
                     <DropdownItem className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none" key="exit">
                         <button onClick={handleLogout} className="flex items-center min-w-[240px] py-2 gap-3">
-                            <LuLogOut/>
+                            <LuLogOut />
                             <span>Sair</span>
                         </button>
                     </DropdownItem>
@@ -75,14 +68,7 @@ export default function AuthUser({ type }: { type: "mobile" | "desktop" }) {
                 <DropdownTrigger className="focus:bg-neutral-900">
                     <button className="outline-none flex gap-2 justify-center items-center tablet:hidden p-2 transition hover:bg-neutral-900 rounded-lg">
                         <span className="font-bold">{user.username}</span>
-                        <Avatar
-                            src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
-                            classNames={{
-                                base: "bg-gradient-to-r from-fuchsia-500 via-purple-500 to-indigo-500",
-                                icon: "text-black/80",
-                            }}
-                            showFallback
-                        />
+                        <Avatar src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} />
                     </button >
                 </DropdownTrigger >
                 <DropdownMenu className="bg-neutral-800 rounded-lg outline-none">
@@ -93,14 +79,14 @@ export default function AuthUser({ type }: { type: "mobile" | "desktop" }) {
                         </Link>
                     </DropdownItem>
                     <DropdownItem className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none" key="exit">
-                        <Link href="/guilds" onClick={handleLogout} className="flex items-center min-w-28 py-2 gap-3">
+                        <Link href="/guilds" className="flex items-center min-w-28 py-2 gap-3">
                             <LuServer />
                             <span>Servidores</span>
                         </Link>
                     </DropdownItem>
                     <DropdownItem className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none" key="exit">
                         <button onClick={handleLogout} className="flex items-center min-w-28 py-2 gap-3">
-                            <LuLogOut/>
+                            <LuLogOut />
                             <span>Sair</span>
                         </button>
                     </DropdownItem>
---
+++ src/components/Mixed/Avatar.tsx
@@ -0,0 +1,15 @@
+import { Avatar as NextUIAvatar } from "@nextui-org/avatar";
+
+export default function Avatar({ src, className }: { src: string, className?: string }) {
+    return (
+        <NextUIAvatar
+            src={src}
+            className={className}
+            classNames={{
+                base: "bg-gradient-to-r from-fuchsia-500 via-purple-500 to-indigo-500",
+                icon: "text-black/80",
+            }}
+            showFallback
+        />
+    )
+}
