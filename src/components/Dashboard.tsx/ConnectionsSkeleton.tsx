42214bb29624197a439ba6e8b44402352c6ca2fe
Spyei <caio.salamao1234@gmail.com>
2024-06-08 14:29:15 -0300

    chore: skeleton fill

--- src/components/Dashboard.tsx/ConnectionsSkeleton.tsx
+++ src/components/Dashboard.tsx/ConnectionsSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@nextui-org/react";
 
 export default function ConnectionsSkeleton({ fill = 4 }) {
     return (
-        Array(4).fill(0).map((_, index) => (
+        Array(fill).fill(0).map((_, index) => (
             <div key={index} className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 transition w-full">
                 <div className="w-16 h-12 rounded-full bg-neutral-700 animate-pulse" />
                 <div className="flex flex-col gap-1 w-full">
