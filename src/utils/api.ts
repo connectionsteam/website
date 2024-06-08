90b0fadbc5de5ecdcd58f3d918e644620610ba9e
Spyei <caio.salamao1234@gmail.com>
2024-06-08 20:48:29 -0300

    fix: application/json axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,6 +4,7 @@ import Cookies from "js-cookie";
 export const api = axios.create({
     baseURL: "/api",
     headers: {
+        "Content-Type": "application/json",
         Authorization: Cookies.get("discord_user"),
     },
 });
