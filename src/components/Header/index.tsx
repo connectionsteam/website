2f3b336be01b4edef4e9c69b96bb00ffa6d8e17a
Spyei <caio.salamao1234@gmail.com>
2024-06-08 11:56:55 -0300

    chore: header layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -30,16 +30,17 @@ export default function Header() {
 
     return (
         <header className="w-screen flex justify-center text-white fixed z-50 tablet:px-2">
-            <div className="mt-2 flex gap-4 justify-between items-center w-full max-w-[1100px] p-1 px-2 tablet:p-2 bg-black bg-opacity-20 backdrop-blur-sm rounded-lg tablet:mr-0">
+            <div className="mt-2 flex gap-4 items-center w-full max-w-[1100px] p-1 px-2 tablet:p-2 bg-black bg-opacity-20 backdrop-blur-sm rounded-lg tablet:mr-0">
                 <Link onClick={handleRecallDrawer} className="transition duration-300" href="/">
                     <h1 className="text-2xl p-2 tablet:p-0 font-bold bg-clip-text text-transparent bg-gradient-to-r from-fuchsia-500 via-purple-500 to-indigo-500">Connections</h1>
                 </Link>
                 <div className="flex gap-4 justify-center text-lg font-bold tablet:hidden">
-                    <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/dashboard"><Underline>Dashboard</Underline></Link>
-                    <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/dashboard"><Underline>Premium</Underline></Link>
-                    <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/dashboard"><Underline>Support</Underline></Link>
+                    <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/premium"><Underline>Premium</Underline></Link>
+                    <a onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="https://discord.gg/RXBRraTWeY" about="_blank"><Underline>Support</Underline></a>
                 </div>
-                <AuthUser type="desktop" />
+                <div className="w-full flex justify-end">
+                    <AuthUser type="desktop" />
+                </div>
                 <button onClick={() => setIsDrawerOpen(true)} className="tabletdesk:hidden">
                     <RiMenu3Line fill="#fff" size={30} />
                 </button>
@@ -47,11 +48,10 @@ export default function Header() {
             <div className={`fixed top-0 right-0 w-full h-full bg-opacity-50 z-40 transition-all transform text-white ${isDrawerOpen ? "translate-x-0 backdrop-blur-sm bg-black bg-opacity-20" : "translate-x-full bg-opacity-0"}`}>
                 <div ref={drawerRef} className="fixed top-0 right-0 w-80 h-full bg-neutral-900 p-6">
                     <button onClick={() => setIsDrawerOpen(false)} className="text-white mb-4 flex justify-end w-full"><FaX size={20} /></button>
-                    <AuthUser type="mobile"/>
+                    <AuthUser type="mobile" />
                     <div className="flex gap-4 justify-center text-lg flex-col">
-                        <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/dashboard"><Underline>Dashboard</Underline></Link>
                         <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/dashboard"><Underline>Premium</Underline></Link>
-                        <Link onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="/dashboard"><Underline>Support</Underline></Link>
+                        <a onClick={handleRecallDrawer} className="transition duration-300 group ease-in-out" href="https://discord.gg/RXBRraTWeY" about="_blank"><Underline>Support</Underline></a>
                     </div>
                 </div>
             </div>
