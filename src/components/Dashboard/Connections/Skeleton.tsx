136d04b5e2afd2397d3caa3ac37525ced85a2e25
Spyei <caio.salamao1234@gmail.com>
2024-06-08 22:43:56 -0300

    feat: connections skeleton

---
+++ src/components/Dashboard/Connections/Skeleton.tsx
@@ -0,0 +1,16 @@
+export default function ConnectionsSkeletonC() {
+    return (
+        <div className="flex flex-col gap-3 w-full">
+            <div className="flex gap-4 w-full flex-col">
+                <span className="font-bold text-xl">Conexão</span>
+                <div className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 w-full">
+                    <div className="w-12 h-12 bg-neutral-700 rounded-full animate-pulse"></div>
+                    <div className="flex flex-col gap-2 text-start">
+                        <div className="bg-neutral-700 h-6 rounded-lg animate-pulse w-36"></div>
+                        <div className="bg-neutral-700 h-4 rounded-lg animate-pulse w-64"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
