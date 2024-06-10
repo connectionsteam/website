b3d7e969e5f5beb2ebac15ecf55cc8de34bf7547
Spyei <caio.salamao1234@gmail.com>
2024-06-10 18:20:03 -0300

    feat: choose language menu

---
+++ src/components/Header/Language.tsx
@@ -0,0 +1,46 @@
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
+import FlagBr from "../Flags/br";
+import FlagEn from "../Flags/en";
+import { useEffect, useState } from "react";
+import { useIsClient } from "@/contexts/Client";
+
+export default function ChooseLanguage() {
+    const isClient = useIsClient();
+    const [language, setLanguage] = useState("en-US");
+    const [selectedLanguage, setSelectedLanguage] = useState(language);
+
+    const handleLanguage = (language: string) => {
+        if (isClient) {
+            localStorage.setItem("language", language);
+        }
+
+        if (language !== selectedLanguage) window.location.reload();
+
+        setSelectedLanguage(language);
+    };
+
+    useEffect(() => {
+        const storedLanguage = localStorage.getItem("language") || "en-US";
+
+        setLanguage(storedLanguage);
+        setSelectedLanguage(storedLanguage);
+    }, []);
+
+    return (
+        <Dropdown className="bg-neutral-800 text-white rounded-lg outline-none">
+            <DropdownTrigger className="focus:bg-neutral-900">
+                <button className="outline-none flex gap-2 justify-center items-center tablet:hidden p-2 transition hover:bg-neutral-900 rounded-lg">
+                    <div className="w-8">{selectedLanguage === "pt-BR" ? <FlagBr /> : <FlagEn />}</div>
+                </button>
+            </DropdownTrigger>
+            <DropdownMenu className="bg-neutral-800 rounded-lg outline-none">
+                <DropdownItem onClick={() => handleLanguage("pt-BR")} className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none w-11">
+                    <FlagBr />
+                </DropdownItem>
+                <DropdownItem onClick={() => handleLanguage("en-US")} className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none w-11 mt-1">
+                    <FlagEn />
+                </DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+    );
+}
