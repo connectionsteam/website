e85f201061b8efbcbadfaa2c779b0b228d2e8d35
Spyei <caio.salamao1234@gmail.com>
2024-06-14 16:48:48 -0300

    feat: edit connection modal

--- src/components/Dashboard/Connections/index.tsx
+++ src/components/Dashboard/Connections/index.tsx
@@ -8,11 +8,20 @@ import ConnectionsSkeleton from "../ConnectionsSkeleton";
 import CreateConnectionForm from "./Connection/FormCreateConnection";
 import { LanguageContext } from "@/contexts/Language";
 import { languages } from "@/locale";
+import ConnectionComponent from "./Connection";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/modal";
 
 export default function ConnectionsComponent() {
     const [connections, setConnections] = useState<ConnectionPayload[] | null>(null);
     const [searchQuery, setSearchQuery] = useState("");
     const { language } = useContext(LanguageContext);
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [connectionId, setConnectionId] = useState("");
+
+    const openModal = (name: string) => {
+        setConnectionId(name);
+        onOpen();
+    };
 
     const handleChangeQuery = (event: ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(event.target.value);
@@ -41,19 +50,30 @@ export default function ConnectionsComponent() {
                 <div className="grid grid-cols-3 gap-3 w-full">
                     {connections ? (
                         connections.filter((connection) => connection.name.toLowerCase().includes(searchQuery.toLowerCase()) || connection.name.includes(searchQuery)).map((connection) => (
-                            <Link href={`/connection/${connection.name.replace(/ /g, "-")}`} key={connection.name} className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 hover:bg-neutral-700 transition">
+                            <button onClick={() => openModal(connection.name)} key={connection.name} className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 hover:bg-neutral-700 transition">
                                 <Avatar className="w-12 h-12" src={connection.icon || ""} key={connection.name} />
                                 <div className="flex flex-col gap-1 text-start">
                                     <span className="font-bold text-lg">{connection.name}</span>
                                     {connection.description && <span className="text-neutral-300 text-sm">{connection.description.length > 30 ? connection.description.slice(0, 30) + "..." : connection.description}</span>}
                                 </div>
-                            </Link>
+                            </button>
                         ))
                     ) : <ConnectionsSkeleton key={Math.random()} />}
-                    <CreateConnectionForm/>
+                    <CreateConnectionForm />
+                    <Modal classNames={{
+                        closeButton: "transition hover:bg-neutral-700",
+                        wrapper: "overflow-y-hidden",
+                        base: "max-h-screen overflow-y-auto",
+                    }} isOpen={isOpen} onOpenChange={onOpenChange}>
+                        <ModalContent className="bg-neutral-800 text-white">
+                            <ModalHeader className="flex flex-col gap-1 bg-neutral-800">Editar conexão {connectionId}</ModalHeader>
+                            <ModalBody>
+                                <ConnectionComponent id={connectionId} />
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
                 </div>
             </div>
         </div>
     );
 }
-
