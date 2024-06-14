20bbfe29641845ae0b14849d0bd587f9105e2a87
Spyei <caio.salamao1234@gmail.com>
2024-06-14 18:31:36 -0300

    chore: protected dashboard skeleton

--- src/components/Dashboard/Connections/Skeleton.tsx
+++ src/components/Dashboard/Connections/Skeleton.tsx
@@ -1,20 +1,48 @@
-export default function ConnectionsSkeletonC() {
+import { Input } from "@nextui-org/input";
+import ConnectionsSkeleton from "../ConnectionsSkeleton";
+import { useContext } from "react";
+import { LanguageContext } from "@/contexts/Language";
+import { languages } from "@/locale";
+import DefaultLayout from "@/components/Mixed/Layout";
+import { Tab, Tabs } from "@nextui-org/tabs";
+
+export default function ConnectionsProtectedSkeleton() {
+    const { language } = useContext(LanguageContext);
+
     return (
-        <div className="p-2 bg-neutral-800 rounded-lg w-full justify-center flex flex-col tablet:max-w-none">
-            <div className="flex items-center justify-center flex-col gap-3 w-full">
-                <div className="flex items-center flex-col justify-center gap-4 w-full">
-                    <div className="h-20 w-20 bg-neutral-700 rounded-full animate-pulse"></div>
-                    <div className="bg-neutral-700 h-8 w-40 rounded-lg animate-pulse"></div>
-                    <div className="transition w-full p-5 rounded-lg bg-neutral-900/50 animate-pulse"></div>
-                </div>
-                <div className="transition w-full p-8 rounded-lg bg-neutral-900/50 animate-pulse"></div>
-                <div className="bg-neutral-700 w-full p-6 rounded-lg animate-pulse"></div>
-                <div className="flex flex-col bg-neutral-700 bg-opacity-10 p-3 rounded-lg w-full gap-2">
-                    <div className="h-5 w-24 bg-neutral-700 rounded-full animate-pulse"></div>
-                    <div className="h-5 w-32 pr-6 bg-neutral-700 rounded-full animate-pulse"></div>
-                    <div className="flex gap-2 tablet:max-w-none justify-center items-center bg-neutral-700 transition p-6 rounded-lg animate-pulse"></div>
-                </div>
+        <DefaultLayout className="mt-24">
+            <div className="flex w-full flex-col items-center">
+                <Tabs classNames={{
+                    cursor: "bg-neutral-700",
+                    tabList: "bg-neutral-800"
+                }} aria-label="Options">
+                    <Tab className="flex items-start w-full" key="connections" title={languages[language].dashboard.connections.title}>
+                        <Skeleton />
+                    </Tab>
+                    <Tab className="flex items-start w-full" key="guilds" title={languages[language].dashboard.guilds.title}>
+                        <Skeleton />
+                    </Tab>
+                </Tabs>
+            </div>
+        </DefaultLayout>
+    );
+}
+
+function Skeleton() {
+    const { language } = useContext(LanguageContext);
+
+    return (
+        <div className="flex w-full items-start flex-col gap-4 z-10 tablet:px-3">
+            <div className="flex flex-col gap-2">
+                <div className="font-bold text-3xl">{languages[language].dashboard.connections.title}</div>
+                <div className="text-neutral-300">{languages[language].dashboard.connections.description}</div>
+            </div>
+            <Input classNames={{
+                inputWrapper: "rounded-lg bg-neutral-800 group-hover:bg-neutral-700",
+            }} type="string" label={languages[language].dashboard.misc.filterConnections} />
+            <div className="grid grid-cols-3 gap-3 w-full">
+                <ConnectionsSkeleton key={Math.random()} />
             </div>
         </div>
-    );
+    )
 }
