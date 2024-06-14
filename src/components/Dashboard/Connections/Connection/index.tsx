c39ee1a0e47a53ae4385c3592ceb5a0d448e1fa2
Spyei <caio.salamao1234@gmail.com>
2024-06-14 18:32:08 -0300

    chore: static get connection and guild

--- src/components/Dashboard/Connections/Connection/index.tsx
+++ src/components/Dashboard/Connections/Connection/index.tsx
@@ -1,33 +1,14 @@
-import { api } from "@/utils/api";
-import { useContext, useEffect, useState } from "react";
 import { ConnectionPayload } from "@/types";
-import ConnectionsSkeletonC from "../Skeleton";
-import { languages } from "@/locale";
-import { LanguageContext } from "@/contexts/Language";
 import EditConnection from "./EditConnection";
 import DeleteConnection from "./DeleteConnection";
-export default function ConnectionComponent({ id }: { id: string }) {
-    const [connection, setConnection] = useState<ConnectionPayload | null>(null);
-    const { language } = useContext(LanguageContext);
 
-    useEffect(() => {
-        if (!id) return;
-
-        const fetchconnection = async () => {
-            const res = await api.get(`/connections/${id}`);
-
-            setConnection(res.data);
-        };
-
-        fetchconnection();
-    }, [id]);
-
-    return connection ? (
+export default function ConnectionComponent({ connection }: { connection: ConnectionPayload }) {
+    return (
         <div className="flex gap-2 w-full flex-col py-2">
             <EditConnection connection={connection} />
             <div className="bg-neutral-800 rounded-lg w-full gap-4 flex flex-col">
                 <DeleteConnection id={connection.name} />
             </div>
         </div>
-    ) : <ConnectionsSkeletonC />;
+    )
 }
--- src/components/Dashboard/Guilds/index.tsx
+++ src/components/Dashboard/Guilds/index.tsx
@@ -1,20 +1,16 @@
 import { Input } from "@nextui-org/input";
 import Link from "next/link";
-import { ChangeEvent, useContext, useEffect, useState } from "react";
-import { api } from "@/utils/api";
+import { ChangeEvent, useContext, useState } from "react";
 import { GuildPayload } from "@/types";
 import Avatar from "@/components/Mixed/Avatar";
 import ConnectionsSkeleton from "../ConnectionsSkeleton";
-import ProtectedRoute from "@/components/Mixed/ProtectedRoute";
-import GuildsSkeleton from "./Skeleton";
 import { LuPlusCircle } from "react-icons/lu";
 import { LanguageContext } from "@/contexts/Language";
 import { languages } from "@/locale";
 
 const url = "https://discord.com/oauth2/authorize?client_id=1243234162077470802";
 
-export default function GuildsComponent() {
-    const [guilds, setGuilds] = useState<GuildPayload[] | null>(null);
+export default function GuildsComponent({ guilds }: { guilds: GuildPayload[] | null }) {
     const [searchQuery, setSearchQuery] = useState("");
     const { language } = useContext(LanguageContext);
 
@@ -22,49 +18,35 @@ export default function GuildsComponent() {
         setSearchQuery(event.target.value);
     };
 
-    useEffect(() => {
-        const fetchConnections = async () => {
-            const response = await api.get("/users/@me/guilds");
-
-            setGuilds(response.data);
-        };
-
-        fetchConnections();
-    }, []);
-
     return (
-        <ProtectedRoute loading={<GuildsSkeleton key={Math.random()} />}>
-            <div className="flex items-center justify-center text-white">
-                <div className="flex w-full max-w-[1100px] items-start flex-col gap-4 z-10 tablet:px-3 mt-28">
-                    <div className="flex flex-col gap-2">
-                        <h1 className="font-bold text-3xl">{languages[language].dashboard.guilds.title}</h1>
-                        <span className="text-neutral-300">{languages[language].dashboard.guilds.description}</span>
-                    </div>
-                    <Input classNames={{
-                        inputWrapper: "rounded-lg bg-neutral-800 group-hover:bg-neutral-700",
-                    }} onChange={handleChangeQuery} type="string" label={languages[language].dashboard.misc.filterGuilds} />
-                    <div className="grid grid-cols-3 gap-3 w-full">
-                        {guilds ? (
-                            guilds.filter((guild) => guild.name.toLowerCase().includes(searchQuery.toLowerCase()) || guild.id.includes(searchQuery)).map((guild) => (
-                                <Link href={`/guild/${guild.id}`} key={guild.id} className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 hover:bg-neutral-700 transition">
-                                    <Avatar className="w-12 h-12" src={guild.icon} key={guild.id} />
-                                    <div className="flex flex-col gap-1 text-start">
-                                        <span className="font-bold text-lg">{guild.name}</span>
-                                        <span className="text-neutral-300 text-sm">{guild.id}</span>
-                                    </div>
-                                </Link>
-                            ))
-                        ) : <ConnectionsSkeleton key={Math.random()} />}
-                        <div className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full">
-                            <a target="_blank" href={url} className="flex items-center justify-center gap-2 p-3 h-full w-full rounded-lg bg-neutral-800 hover:bg-neutral-700 transition">
-                                <LuPlusCircle size={20} />
-                                <span>{languages[language].dashboard.guilds.addServer}</span>
-                            </a>
-                        </div>
-                    </div>
+        <div className="flex w-full items-start flex-col gap-4 z-10 tablet:px-3">
+            <div className="flex flex-col gap-2">
+                <h1 className="font-bold text-3xl">{languages[language].dashboard.guilds.title}</h1>
+                <span className="text-neutral-300">{languages[language].dashboard.guilds.description}</span>
+            </div>
+            <Input classNames={{
+                inputWrapper: "rounded-lg bg-neutral-800 group-hover:bg-neutral-700",
+            }} onChange={handleChangeQuery} type="string" label={languages[language].dashboard.misc.filterGuilds} />
+            <div className="grid grid-cols-3 gap-3 w-full">
+                {guilds ? (
+                    guilds.filter((guild) => guild.name.toLowerCase().includes(searchQuery.toLowerCase()) || guild.id.includes(searchQuery)).map((guild) => (
+                        <Link href={`/guild/${guild.id}`} key={guild.id} className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 hover:bg-neutral-700 transition">
+                            <Avatar className="w-12 h-12" src={guild.icon} key={guild.id} />
+                            <div className="flex flex-col gap-1 text-start">
+                                <span className="font-bold text-lg">{guild.name}</span>
+                                <span className="text-neutral-300 text-sm">{guild.id}</span>
+                            </div>
+                        </Link>
+                    ))
+                ) : <ConnectionsSkeleton key={Math.random()} />}
+                <div className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full">
+                    <a target="_blank" href={url} className="flex items-center justify-center gap-2 p-3 h-full w-full rounded-lg bg-neutral-800 hover:bg-neutral-700 transition">
+                        <LuPlusCircle size={20} />
+                        <span>{languages[language].dashboard.guilds.addServer}</span>
+                    </a>
                 </div>
             </div>
-        </ProtectedRoute>
+        </div>
     );
 }
 
