ab11a864dbacfd1ec0bbb64d4f0cbebe5183c84b
Spyei <caio.salamao1234@gmail.com>
2024-06-13 22:16:46 -0300

    feat: next ui switch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(avatar|input|modal|tabs).js"
+    "./node_modules/@nextui-org/theme/dist/components/(avatar|input|modal|toggle|tabs).js"
   ],
     theme: {
         extend: {
