09244b8d0b35ee5ba4358ea8f9fdb88dbc906b0b
Spyei <caio.salamao1234@gmail.com>
2024-06-14 18:31:04 -0300

    chore: adding classname to default layout

--- src/components/Mixed/Layout.tsx
+++ src/components/Mixed/Layout.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
 
-export default function DefaultLayout({ children }: { children: ReactNode }) {
+export default function DefaultLayout({ children, className }: { children: ReactNode, className?: string }) {
     return (
         <div className="flex items-center justify-center text-white">
-            <div className="flex w-full max-w-[1100px] items-start flex-col gap-4 z-10 tablet:px-3 mt-28">
+            <div className={`${className || ""} flex w-full max-w-[1100px] items-start flex-col gap-4 z-10 tablet:px-3 mt-28`}>
                 {children}
             </div>
         </div>
