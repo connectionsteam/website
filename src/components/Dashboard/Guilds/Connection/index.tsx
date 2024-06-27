6a90b78d2f59bfd5ddf1ed916e4f2e3aa1fc09f3
Spyei <caio.salamao1234@gmail.com>
2024-06-26 23:38:21 -0300

    chore: new edit connection body

--- src/components/Dashboard/Guilds/Connection/index.tsx
+++ src/components/Dashboard/Guilds/Connection/index.tsx
@@ -1,9 +1,52 @@
+import Avatar from "@/components/Mixed/Avatar";
 import { ConnectedConnectionPayload, GuildPayload } from "@/types";
+import BlockedWords from "./BlockedWords";
+import { useState } from "react";
 
-export default function GuildEditConnection({ connection, guild }: { connection: ConnectedConnectionPayload, guild: GuildPayload }) {
-    return (
-        <div>
-            <h1>Editar coneão {connection.name}</h1>
-        </div>
-    )
+interface Props {
+    guild: GuildPayload;
+    GuildConnection: ConnectedConnectionPayload;
+    setGuild: (guild: GuildPayload) => void;
 }
+
+export default function GuildEditConnection({ GuildConnection, guild, setGuild }: Props) {
+    const [connection, setConnection] = useState(GuildConnection);
+    
+    return (
+        <div className="w-full rounded-lg bg-neutral-800 p-6 transition flex flex-col gap-4">
+            <h1 className="font-bold text-xl">Editar conexão conectada</h1>
+            <div className="flex flex-col gap-6">
+                <div className="flex gap-3">
+                    <Avatar className="w-16 h-16" src={connection.icon || ""} />
+                    <div className="flex gap-1 flex-col">
+                        <span className="font-bold text-lg">{connection.name}</span>
+                        <div className="text-neutral-300">{connection.description}</div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h1 className="font-bold text-xl">Informações</h1>
+                    <div className="flex flex-col gap-1">
+                        <div className="flex items-center gap-1">
+                            <div className="">Palavras bloqueadas:</div>
+                            <span className="text-neutral-300">
+                                {connection.blockwords.length > 0 ? connection.blockwords.join(", ") : "Nenhuma"}
+                            </span>
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <div className="">Flags:</div>
+                            <span className="text-neutral-300">
+                                {connection.flags.length > 0 ? connection.flags.join(", ") : "Nenhuma"}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h1 className="font-bold text-xl">Configurar</h1>
+                    <div className="flex flex-col gap-1">
+                        <BlockedWords setGuild={setGuild} setConnection={setConnection} guild={guild} connection={connection} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
