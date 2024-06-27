ee65c76087211d17577c1baae3faac71a152ddef
Spyei <caio.salamao1234@gmail.com>
2024-06-26 23:37:22 -0300

    chore: better default input

--- src/components/Mixed/Input.tsx
+++ src/components/Mixed/Input.tsx
@@ -1,14 +1,28 @@
 import { ChangeEventHandler } from "react";
 
-export default function DefaultInput({ label, type, placeholder, onChange, obrigatory }: { obrigatory?: boolean, label: string, type: string, placeholder: string, onChange: ChangeEventHandler<HTMLInputElement> }) {
+interface Props {
+    label: string;
+    type: string;
+    placeholder: string;
+    onChange: ChangeEventHandler<HTMLInputElement>;
+    obrigatory?: boolean;
+    value?: string;
+}
+
+export default function DefaultInput({ label, type, placeholder, onChange, obrigatory, value }: Props) {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-full">
             <label className="text-neutral-300 flex gap-1">
                 <div>{label}</div>
                 {obrigatory && <span className="text-red-500">*</span>}
             </label>
             <div className="w-full">
-                <input className="transition w-full p-3 rounded-lg bg-neutral-900/50 focus:outline-none" onChange={onChange} type={type} placeholder={placeholder} />
+                <input
+                    value={value}
+                    className="transition w-full p-3 rounded-lg bg-neutral-900/50 focus:outline-none" 
+                    onChange={onChange} 
+                    type={type} 
+                    placeholder={placeholder} />
             </div>
         </div>
     );
