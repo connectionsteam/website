598b2f966cdf06eefd0ae4880992f6c8b873ec24
Spyei <caio.salamao1234@gmail.com>
2024-06-27 21:54:43 -0300

    chore: adding a chars counter

--- src/components/Mixed/Input.tsx
+++ src/components/Mixed/Input.tsx
@@ -4,25 +4,44 @@ interface Props {
     label: string;
     type: string;
     placeholder: string;
-    onChange: ChangeEventHandler<HTMLInputElement>;
+    onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
     obrigatory?: boolean;
     value?: string;
+    textArea?: boolean;
+    maxChars?: number;
+    minChars?: number;
 }
 
-export default function DefaultInput({ label, type, placeholder, onChange, obrigatory, value }: Props) {
+export default function DefaultInput({ label, type, placeholder, onChange, obrigatory, value, textArea, maxChars, minChars }: Props) {
     return (
         <div className="flex flex-col gap-2 w-full">
             <label className="text-neutral-300 flex gap-1">
                 <div>{label}</div>
                 {obrigatory && <span className="text-red-500">*</span>}
             </label>
-            <div className="w-full">
-                <input
-                    value={value}
-                    className="transition w-full p-3 rounded-lg bg-neutral-900/50 focus:outline-none" 
-                    onChange={onChange} 
-                    type={type} 
-                    placeholder={placeholder} />
+            <div className="relative w-full">
+                {textArea ? (
+                    <textarea
+                        value={value}
+                        maxLength={(minChars && maxChars) ? maxChars + minChars : maxChars || 9999}
+                        className={`transition w-full p-3 rounded-lg bg-neutral-900/50 focus:outline-none ${maxChars && "pb-4"}`}
+                        onChange={onChange}
+                        placeholder={placeholder}
+                    />
+                ) : (
+                    <input
+                        value={value}
+                        maxLength={(minChars && maxChars) ? maxChars + minChars : maxChars || 9999}
+                        className={`transition w-full p-3 rounded-lg bg-neutral-900/50 focus:outline-none ${maxChars && "pb-4"}`} 
+                        onChange={onChange}
+                        type={type}
+                        placeholder={placeholder}
+                    />
+                )}
+                {(value && maxChars) && <span className="absolute right-1 bottom-1 text-xs text-neutral-500">
+                    {minChars ? value.length - minChars : value.length}/{maxChars}
+                </span>
+                }
             </div>
         </div>
     );
