840907f819f31996a7013c9c58338108b3e7e638
Spyei <caio.salamao1234@gmail.com>
2024-06-29 17:13:39 -0300

    feat: ice effect in frozen connections

--- src/components/Dashboard/Guilds/Connection/BlockedWords.tsx
+++ src/components/Dashboard/Guilds/Connection/BlockedWords.tsx
@@ -1,10 +1,11 @@
-import { ConnectedConnectionPayload, GuildPayload } from "@/types";
+import { ConnectedConnectionFlags, ConnectedConnectionPayload, GuildPayload } from "@/types";
 import { api } from "@/utils/api";
 import { ChangeEvent, KeyboardEvent, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { BiX } from "react-icons/bi";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FaCheckCircle } from "react-icons/fa";
+import Image from "next/image";
 
 interface Props {
     guild: GuildPayload;
@@ -28,7 +29,7 @@ export default function BlockedWords({ connection, guild, setConnection, setGuil
                 ...connection,
                 blockwords: words
             });
-            
+
             setGuild({
                 ...guild,
                 connections: [
@@ -82,43 +83,53 @@ export default function BlockedWords({ connection, guild, setConnection, setGuil
                     Digite abaixo palavras que você deseja bloquear, separadas por vírgulas (supimpa, carambolas, etc)
                 </span>
             </div>
-            <div className="flex gap-2 flex-col">
-                <div className="flex flex-wrap gap-2 p-3 rounded-lg bg-neutral-900 min-h-14">
-                    <AnimatePresence>
-                        {words.map((word, index) => (
-                            <motion.div
-                                key={index}
-                                initial={{ opacity: 0, x: -30 }}
-                                animate={{ opacity: 1, x: 0 }}
-                                className="px-3 p-1 rounded-lg bg-neutral-700 flex items-center gap-2"
-                            >
-                                <span>{word}</span>
-                                <button onClick={() => removeWord(word)}>
-                                    <BiX
-                                        className="hover:fill-red-500 hover:bg-neutral-800 transition rounded-full font-bold"
-                                        size={23}
-                                    />
-                                </button>
-                            </motion.div>
-                        ))}
-                    </AnimatePresence>
-                    <input
-                        className="outline-none bg-transparent"
-                        placeholder="Digite aqui alguma palavra."
-                        type="text"
-                        value={word}
-                        onKeyDown={handleKeyDown}
-                        onChange={addNewWord}
-                    />
+            <div className="w-full relative">
+                {connection.flags.includes(ConnectedConnectionFlags.Frozen) && (
+                    <>
+                        <div className="bg-gradient-to-tr from-cyan-300 via-sky-200 to-sky-500 absolute z-10 opacity-80 w-[25%] h-full rounded-l-lg"></div>
+                        <div className="bg-gradient-to-br from-sky-500 via-sky-200 to-cyan-300 absolute z-10 opacity-80 left-[25%] w-[25%] h-full"></div>
+                        <div className="bg-gradient-to-tr from-cyan-300 via-sky-200 to-sky-500 absolute z-10 opacity-80 left-[50%] w-[25%] h-full"></div>
+                        <div className="bg-gradient-to-br from-sky-500 via-sky-200 to-cyan-300 absolute z-10 opacity-80 left-[75%] w-[25%] h-full rounded-r-lg"></div>
+                    </>
+                )}
+                <div className="flex gap-2 flex-col">
+                    <div className="flex flex-wrap gap-2 p-3 rounded-lg bg-neutral-900 min-h-14">
+                        <AnimatePresence>
+                            {words.map((word, index) => (
+                                <motion.div
+                                    key={index}
+                                    initial={{ opacity: 0, x: -30 }}
+                                    animate={{ opacity: 1, x: 0 }}
+                                    className="px-3 p-1 rounded-lg bg-neutral-700 flex items-center gap-2"
+                                >
+                                    <span>{word}</span>
+                                    <button onClick={() => removeWord(word)}>
+                                        <BiX
+                                            className="hover:fill-red-500 hover:bg-neutral-800 transition rounded-full font-bold"
+                                            size={23}
+                                        />
+                                    </button>
+                                </motion.div>
+                            ))}
+                        </AnimatePresence>
+                        <input
+                            className="outline-none bg-transparent"
+                            placeholder="Digite aqui alguma palavra."
+                            type="text"
+                            value={word}
+                            onKeyDown={handleKeyDown}
+                            onChange={addNewWord}
+                        />
+                    </div>
+                    <button
+                        className="p-3 bg-neutral-900 transition hover:bg-neutral-900/50 rounded-lg max-w-28 justify-center flex gap-2 items-center"
+                        onClick={handleSaveWords}
+                    >
+                        <span>Salvar</span>
+                        {loading.loading && <AiOutlineLoading3Quarters className="animate-spin" size={18} />}
+                        {loading.check && <FaCheckCircle className="text-green-500" size={18} />}
+                    </button>
                 </div>
-                <button
-                    className="p-3 bg-neutral-900 transition hover:bg-neutral-900/50 rounded-lg max-w-28 justify-center flex gap-2 items-center"
-                    onClick={handleSaveWords}
-                >
-                    <span>Salvar</span>
-                    {loading.loading && <AiOutlineLoading3Quarters className="animate-spin" size={18} />}
-                    {loading.check && <FaCheckCircle className="text-green-500" size={18} />}
-                </button>
             </div>
         </div>
     );
--- src/components/Dashboard/Guilds/Connection/index.tsx
+++ src/components/Dashboard/Guilds/Connection/index.tsx
@@ -1,7 +1,9 @@
 import Avatar from "@/components/Mixed/Avatar";
-import { ConnectedConnectionPayload, GuildPayload } from "@/types";
+import { ConnectedConnectionFlags, ConnectedConnectionPayload, GuildPayload } from "@/types";
 import BlockedWords from "./BlockedWords";
 import { useState } from "react";
+import GuildConnectionFlags from "./Flags";
+import Image from "next/image";
 
 interface Props {
     guild: GuildPayload;
@@ -13,39 +15,50 @@ export default function GuildEditConnection({ GuildConnection, guild, setGuild }
     const [connection, setConnection] = useState(GuildConnection);
 
     return (
-        <div className="w-full rounded-lg bg-neutral-800 p-6 transition flex flex-col gap-4">
-            <h1 className="font-bold text-xl">Editar conexão conectada</h1>
-            <div className="flex flex-col gap-6">
-                <div className="flex gap-3">
-                    <Avatar className="w-16 h-16" src={connection.icon || ""} />
-                    <div className="flex gap-1 flex-col">
-                        <span className="font-bold text-lg">{connection.name}</span>
-                        <div className="text-neutral-300">{connection.description}</div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h1 className="font-bold text-xl">Informações</h1>
-                    <div className="flex flex-col gap-1">
-                        <div className="flex items-center gap-1">
-                            <div className="">Palavras bloqueadas:</div>
-                            <span className="text-neutral-300">
-                                {connection.blockwords
-                                    && connection.blockwords.length > 0 ? connection.blockwords.join(", ") : "Nenhuma"
-                                }
-                            </span>
-                        </div>
-                        <div className="flex items-center gap-1">
-                            <div className="">Flags:</div>
-                            <span className="text-neutral-300">
-                                {connection.flags.length > 0 ? connection.flags.join(", ") : "Nenhuma"}
-                            </span>
+        <div className="relative w-full">
+            {connection.flags.includes(ConnectedConnectionFlags.Frozen) && (
+                <Image 
+                    width={200} 
+                    height={50} 
+                    src="/backgrounds/frozenborder.png" 
+                    alt="Frozen border" 
+                    className="absolute -top-5 -left-4 z-0" />
+            )}
+            <div className="w-full rounded-lg bg-neutral-800 p-6 transition flex flex-col gap-4">
+                <h1 className="font-bold text-xl">Editar conexão conectada</h1>
+                <div className="flex flex-col gap-6">
+                    <div className="flex gap-3">
+                        <Avatar className="w-16 h-16" src={connection.icon || ""} />
+                        <div className="flex gap-1 flex-col">
+                            <span className="font-bold text-lg">{connection.name}</span>
+                            <div className="text-neutral-300">{connection.description}</div>
                         </div>
                     </div>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h1 className="font-bold text-xl">Configurar</h1>
-                    <div className="flex flex-col gap-1">
-                        <BlockedWords setGuild={setGuild} setConnection={setConnection} guild={guild} connection={connection} />
+                    <div className="flex flex-col gap-2">
+                        <h1 className="font-bold text-xl">Informações</h1>
+                        <div className="flex flex-col gap-1">
+                            <div className="flex items-center gap-1">
+                                <div className="">Palavras bloqueadas:</div>
+                                <span className="text-neutral-300">
+                                    {connection.blockwords
+                                        && connection.blockwords.length > 0 ? connection.blockwords.join(", ") : "Nenhuma"
+                                    }
+                                </span>
+                            </div>
+                            <div className="flex gap-1">
+                                <div className="text-start">Flags:</div>
+                                <span className="text-neutral-300">
+                                    {connection.flags.length > 0 ? connection.flags.join(", ") : "Nenhuma"}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <h1 className="font-bold text-xl">Configurar</h1>
+                        <div className="flex flex-col gap-3">
+                            <BlockedWords setGuild={setGuild} setConnection={setConnection} guild={guild} connection={connection} />
+                            <GuildConnectionFlags setGuild={setGuild} setConnection={setConnection} guild={guild} connection={connection} />
+                        </div>
                     </div>
                 </div>
             </div>
