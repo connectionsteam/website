7dc1d57a7a768a657ed16a3b5698b8c5a1289e8c
Spyei <caio.salamao1234@gmail.com>
2024-07-04 12:07:53 -0300

    feat: implementing threads

--- src/components/Dashboard/Guilds/Guild/Infos.tsx
+++ src/components/Dashboard/Guilds/Guild/Infos.tsx
@@ -1,13 +1,17 @@
 import Avatar from "@/components/Mixed/Avatar";
-import { GuildPayload } from "@/types";
+import { GuildChannelsPayload, GuildPayload, GuildThreadsPayload } from "@/types";
 import GuildMods from "./Mods";
+import Threads from "./Threads";
 
 interface Props {
     guild: GuildPayload;
     setGuild: (guild: GuildPayload) => void;
+    threads: GuildThreadsPayload[];
+    channels: GuildChannelsPayload[];
+    setThreads: (threads: GuildThreadsPayload[]) => void;
 }
 
-export default function Infos({ guild, setGuild }: Props) {
+export default function Infos({ guild, setGuild, threads, channels, setThreads }: Props) {
     return (
         <div className="w-full rounded-lg bg-neutral-800 p-6 transition flex flex-col gap-4">
             <h1 className="font-semibold text-xl">Informações</h1>
@@ -19,6 +23,7 @@ export default function Infos({ guild, setGuild }: Props) {
                         <div className="text-sm text-neutral-300">{guild.id}</div>
                     </div>
                 </div>
+                {(threads.length > 0  || !threads) && <Threads setThreads={setThreads} channels={channels} guild={guild} threads={threads} />}
                 <GuildMods setGuild={setGuild} guild={guild} />
             </div>
         </div>
