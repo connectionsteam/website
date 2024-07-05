b45b3f97a980e0c92a9d1bccee3a9053e3914fe6
Spyei <caio.salamao1234@gmail.com>
2024-07-05 19:18:29 -0300

    chore: removing tabs

--- src/components/Dashboard/Guilds/Guild/index.tsx
+++ src/components/Dashboard/Guilds/Guild/index.tsx
@@ -1,43 +1,62 @@
 "use client"
 import DefaultLayout from "@/components/Mixed/Layout";
-import { GuildChannelsPayload, GuildPayload, GuildThreadsPayload, TabsStructure } from "@/types";
+import { ConnectedConnectionPayload, GuildChannelsPayload, GuildPayload, GuildThreadsPayload, TabState } from "@/types";
 import { api } from "@/utils/api";
 import { useRouter } from "next/router";
-import { MouseEvent, useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import GuildSkeleton from "./Skeleton";
 import Infos from "./Infos";
-import Connections from "./Connecions";
-import { BiX } from "react-icons/bi";
 import Channels from "./Channels";
+import Connections from "./Connecions";
+import GuildEditConnection from "../Connection";
 
 export default function GuildComponent() {
     const router = useRouter();
     const { id } = router.query;
     const [guild, setGuild] = useState<GuildPayload>();
-    const [tabs, setTabs] = useState<TabsStructure[]>([]);
-    const [selectedTab, setSelectedTab] = useState("infos");
     const [channels, setChannels] = useState<GuildChannelsPayload[]>([]);
     const [threads, setThreads] = useState<GuildThreadsPayload[]>([]);
+    const [connection, setConnection] = useState<ConnectedConnectionPayload>(null!);
 
-    const addTab = (newTab: TabsStructure) => {
-        setTabs((prevTabs) => {
-            if (prevTabs.some(tab => tab.value === newTab.value)) return prevTabs;
+    const [tab, setTab] = useState<TabState>({
+        tabs: [],
+        selected: "infos",
+        connection: false,
+    });
 
-            return [...prevTabs, newTab];
-        });
+    const handleSelectConnection = async (connection: ConnectedConnectionPayload) => {
+        setTab({ ...tab, connection: true, selected: connection.name });
+        setConnection(connection);
     };
 
-    const removeTab = (tab: TabsStructure, event: MouseEvent<HTMLButtonElement>) => {
-        event.stopPropagation();
+    const handleChangeTab = (selected: string) => {
+        if (connection && selected === "connections")
+            return setTab({
+                connection: false, selected,
+                tabs: [
+                    ...tab.tabs.filter((t) => t.value !== "connections"),
+                    {
+                        value: "connections",
+                        title: "Conexões",
+                        content: <Connections
+                            handleSelectConnection={handleSelectConnection}
+                            connection={connection}
+                            setConnection={setConnection}
+                            key={0}
+                            channels={channels}
+                            setGuild={setGuild}
+                            guild={guild as GuildPayload}
+                        />
+                    }
+                ]
+            });
 
-        setTabs((prevTabs) => {
-            return prevTabs.filter(prevTab => prevTab.value !== tab.value);
+        setTab({
+            ...tab,
+            connection: false,
+            selected
         });
-
-        setTimeout(() => {
-            setSelectedTab("connections");
-        }, 100);
     };
 
     useEffect(() => {
@@ -57,67 +76,82 @@ export default function GuildComponent() {
 
     useEffect(() => {
         if (guild && threads) {
-            setTabs([
-                {
-                    value: "infos",
-                    title: "Informações",
-                    content: <Infos setGuild={setGuild} setThreads={setThreads} channels={channels} threads={threads} key={0} guild={guild} />,
-                },
-                {
-                    value: "channels", 
-                    title: "Canais",
-                    content: <Channels setGuild={setGuild} guild={guild} channels={channels} key={0} />,
-                },
-                {
-                    value: "connections",
-                    title: "Conexões",
-                    content: <Connections
-                        key={0}
-                        channels={channels}
-                        setGuild={setGuild}
-                        setSelectedTab={setSelectedTab}
-                        guild={guild}
-                        addTab={addTab}
-                    />,
-                },
-                ...tabs.filter((tab) => !["connections", "infos", "channels"].includes(tab.value)),
-            ]);
+            setTab({
+                tabs: [
+                    {
+                        value: "infos",
+                        title: "Informações",
+                        content: <Infos
+                            setGuild={setGuild}
+                            setThreads={setThreads}
+                            channels={channels}
+                            threads={threads}
+                            key={0}
+                            guild={guild}
+                        />,
+                    },
+                    {
+                        value: "channels",
+                        title: "Canais",
+                        content: <Channels setGuild={setGuild} guild={guild} channels={channels} key={0} />,
+                    },
+                    {
+                        value: "connections",
+                        title: connection?.name || "Conexões",
+                        content: <Connections
+                            handleSelectConnection={handleSelectConnection}
+                            connection={connection}
+                            setConnection={setConnection}
+                            key={0}
+                            channels={channels}
+                            setGuild={setGuild}
+                            guild={guild}
+                        />,
+                    }
+                ],
+                selected: tab.selected,
+                connection: tab.connection,
+            });
         }
-    }, [guild, threads, channels]);
+    }, [guild, threads, channels, connection]);
 
     return (
         <DefaultLayout>
             {guild ? (
                 <div className="flex flex-col gap-4 w-full">
                     <div className="flex mb-1 bg-neutral-800 rounded-lg p-1 overflow-x-auto">
-                        {tabs.map((tab) => (
+                        {tab.tabs.map((t) => (
                             <button
-                                key={tab.value}
-                                onClick={() => setSelectedTab(tab.value)}
-                                className={`text-white px-4 rounded-lg py-2 cursor-pointer transition-colors duration-300 gap-2 flex ${selectedTab === tab.value ? "bg-neutral-700" : ""}`}
+                                key={t.value}
+                                onClick={() => handleChangeTab(t.value)}
+                                className={`text-white px-4 rounded-lg py-2 cursor-pointer transition-colors duration-300 gap-2 flex ${(tab.connection && t.value === "connections") ? "bg-neutral-700" : ""} ${tab.selected === t.value ? "bg-neutral-700" : ""}`}
                             >
-                                <span>{tab.title}</span>
-                                {!["infos", "connections" ,"channels"].includes(tab.value) && (
-                                    <button
-                                        onClick={(event) => removeTab(tab, event)}
-                                        className="flex items-center justify-center h-full text-neutral-300 hover:text-neutral-400 transition-colors duration-300">
-                                        <BiX
-                                            className="hover:fill-red-500 hover:bg-neutral-800 transition rounded-full font-bold"
-                                            size={23}
-                                        />
-                                    </button>
-                                )}
+                                <span>{t.title}</span>
                             </button>
                         ))}
                     </div>
                     <div>
-                        {tabs.map((tab) => (
-                            selectedTab === tab.value && (
-                                <motion.div key={tab.value} initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }}>
-                                    {tab.content}
-                                </motion.div>
-                            )
-                        ))}
+                        <AnimatePresence>
+                            {tab.tabs.map((t) => (
+                                tab.selected === t.value ? (
+                                    <motion.div key={t.value} initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }}>
+                                        {t.content}
+                                    </motion.div>
+                                ) : tab.connection && t.value === "connections" ? (
+                                    <motion.div key={t.value} initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }}>
+                                        <GuildEditConnection
+                                            handleChangeTab={handleChangeTab}
+                                            connection={connection}
+                                            channels={channels}
+                                            key={connection.name}
+                                            setGuild={setGuild}
+                                            guild={guild as GuildPayload}
+                                            setConnection={setConnection}
+                                        />
+                                    </motion.div>
+                                ) : null)
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
             ) : (
