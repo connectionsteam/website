ff60bf9a6e67264386f6a07f5175b3e147d736c1
Spyei <caio.salamao1234@gmail.com>
2024-07-05 10:58:27 -0300

    feat: use language hook

---
+++ src/hooks/useLanguage.ts
@@ -0,0 +1,9 @@
+import { LanguageContext } from "@/contexts/Language";
+import { languages } from "@/locale";
+import { useContext } from "react";
+
+export function useLanguage() {
+    const { language } = useContext(LanguageContext);
+    
+    return languages[language];
+}
