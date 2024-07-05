8a25e69d59eb646ddb24209997f0637148ac38b9
Spyei <caio.salamao1234@gmail.com>
2024-07-05 10:43:50 -0300

    feat: displaying the connected channel

--- src/components/Dashboard/Guilds/Connection/index.tsx
+++ src/components/Dashboard/Guilds/Connection/index.tsx
@@ -1,27 +1,29 @@
 import Avatar from "@/components/Mixed/Avatar";
-import { ConnectedConnectionFlags, ConnectedConnectionPayload, GuildPayload } from "@/types";
+import { ConnectedConnectionFlags, ConnectedConnectionPayload, GuildChannelsPayload, GuildPayload } from "@/types";
 import BlockedWords from "./BlockedWords";
 import { useState } from "react";
 import GuildConnectionFlags from "./Flags";
 import Image from "next/image";
+import { HiHashtag } from "react-icons/hi";
 
 interface Props {
     guild: GuildPayload;
     GuildConnection: ConnectedConnectionPayload;
     setGuild: (guild: GuildPayload) => void;
+    channels: GuildChannelsPayload[];
 }
 
-export default function GuildEditConnection({ GuildConnection, guild, setGuild }: Props) {
+export default function GuildEditConnection({ GuildConnection, guild, setGuild, channels }: Props) {
     const [connection, setConnection] = useState(GuildConnection);
 
     return (
         <div className="relative w-full">
             {connection.flags.includes(ConnectedConnectionFlags.Frozen) && (
-                <Image 
-                    width={200} 
-                    height={50} 
-                    src="/backgrounds/frozenborder.png" 
-                    alt="Frozen border" 
+                <Image
+                    width={200}
+                    height={50}
+                    src="/backgrounds/frozenborder.png"
+                    alt="Frozen border"
                     className="absolute -top-5 -left-4 z-0" />
             )}
             <div className="w-full rounded-lg bg-neutral-800 p-6 transition flex flex-col gap-4">
@@ -38,7 +40,7 @@ export default function GuildEditConnection({ GuildConnection, guild, setGuild }
                         <h1 className="font-bold text-xl">Informações</h1>
                         <div className="flex flex-col gap-1">
                             <div className="flex items-center gap-1">
-                                <div className="">Palavras bloqueadas:</div>
+                                <div>Palavras bloqueadas:</div>
                                 <span className="text-neutral-300">
                                     {connection.blockwords
                                         && connection.blockwords.length > 0 ? connection.blockwords.join(", ") : "Nenhuma"
@@ -50,6 +52,13 @@ export default function GuildEditConnection({ GuildConnection, guild, setGuild }
                                 <span className="text-neutral-300">
                                     {connection.flags.length > 0 ? connection.flags.join(", ") : "Nenhuma"}
                                 </span>
+                            </div>
+                            <div className="flex gap-1">
+                                <div className="text-start">Canal:</div>
+                                <div className="text-neutral-300 flex gap-2 bg-neutral-900/50 rounded-lg p-1 text-sm items-center">
+                                    <HiHashtag/>
+                                    <span>{channels.find((channel) => channel.id === connection.channelId)?.name}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
