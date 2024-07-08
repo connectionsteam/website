b4eef8aacb90659c05ac946cc8bb3ff31ec089f8
Spyei <caio.salamao1234@gmail.com>
2024-07-08 12:07:06 -0300

    chore: threads layout

--- src/components/Dashboard/Guilds/Guild/Threads/index.tsx
+++ src/components/Dashboard/Guilds/Guild/Threads/index.tsx
@@ -6,7 +6,6 @@ import { api } from "@/utils/api";
 import { useLanguage } from "@/hooks/useLanguage";
 
 interface Props {
-    channels: GuildChannelsPayload[];
     setThreads: (threads: GuildThreadsPayload[]) => void;
     threads: GuildThreadsPayload[];
     guild: GuildPayload;
@@ -16,19 +15,33 @@ interface ThreadProps extends Props {
     thread: GuildThreadsPayload;
 }
 
-export default function Threads({ threads, setThreads, channels, guild }: Props) {
+export default function Threads({ threads, setThreads, guild }: Props) {
+    const l = useLanguage();
+
     return (
-        <div className="flex flex-col gap-2">
-            <h1 className="font-semibold text-xl">Threads</h1>
-            <div className="grid grid-cols-2 tablet:grid-cols-1 gap-2">
-                {threads.map((thread) => <Thread guild={guild} threads={threads} channels={channels} key={thread.id} thread={thread} setThreads={setThreads} />)}
+        <div className="flex flex-col gap-2 w-full">
+            <div className="flex flex-col gap-1">
+                <div className="flex gap-2 items-end">
+                    <h1 className="font-semibold text-xl">Threads</h1>
+                    <span className="text-neutral-300">{threads.length}</span>
+                </div>
+                <span className="text-neutral-300 max-w-96 tablet:max-w-full">{l.dashboard.guilds.threads.description}</span>
+            </div>
+            <div className="flex flex-col gap-3 h-full">
+                {threads.length > 0 ? (
+                    threads.map((thread) => <Thread guild={guild} threads={threads} key={thread.id} thread={thread} setThreads={setThreads} />)
+                ) : (
+                    <div className="flex flex-col gap-2 h-full w-full">
+                        <h1 className="font-bold text-xl mobile:text-lg">{l.dashboard.guilds.threads.thread.noThreads}</h1>
+                        <span>{l.dashboard.guilds.threads.thread.noThreadsDescription}</span>
+                    </div>
+                )}
             </div>
         </div>
     );
 }
 
-function Thread({ thread, setThreads, channels, threads, guild }: ThreadProps) {
-    const threadChannels = channels.filter((channel) => thread.children.includes(channel.id));
+function Thread({ thread, setThreads, threads, guild }: ThreadProps) {
     const l = useLanguage();
 
     const [menu, setMenu] = useState<MenuProps>({
@@ -70,21 +83,21 @@ function Thread({ thread, setThreads, channels, threads, guild }: ThreadProps) {
             />
             <div className="flex gap-2 items-center bg-neutral-900/50 rounded-lg p-3 break-all">
                 <div className="flex flex-col gap-1">
-                    <div className="flex gap-1">
+                    <div className="flex gap-1 mobile:flex-col">
                         <div className="font-semibold">{l.dashboard.guilds.threads.thread.originId}:</div>
-                        <span>{thread.originId}</span>
+                        <span className="text-neutral-300">{thread.originId}</span>
                     </div>
-                    <div className="flex gap-1">
+                    <div className="flex gap-1 mobile:flex-col">
                         <div className="font-semibold">{l.dashboard.guilds.threads.thread.creatorId}:</div>
-                        <span>{thread.creatorId}</span>
+                        <span className="text-neutral-300">{thread.creatorId}</span>
                     </div>
-                    <div className="flex items-start text-start gap-2">
+                    <div className="flex items-start text-start gap-2 mobile:flex-col">
                         <span className="font-semibold">{l.dashboard.guilds.threads.thread.channels}:</span>
-                        {threadChannels.map((channel) => channel.name).join(", ")}
+                        <span className="text-neutral-300">{thread.children.join(", ")}</span>
                     </div>
-                    <div className="text-sm text-neutral-300 flex gap-1">
+                    <div className="text-sm text-neutral-300 flex gap-1 mobile:flex-col">
                         <div className="font-semibold">{l.dashboard.guilds.threads.thread.created}:</div>
-                        <span>{new Date(thread.createdTimestamp).toLocaleString()}</span>
+                        <span className="text-neutral-300">{new Date(thread.createdTimestamp).toLocaleString()}</span>
                     </div>
                 </div>
             </div>
