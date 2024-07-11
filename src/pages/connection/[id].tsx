1255662cd3c4ce64aaaffa6355bd530cd780526d
Spyei <caio.salamao1234@gmail.com>
2024-07-10 22:42:02 -0300

    feat: connection page

---
+++ src/pages/connection/[id].tsx
@@ -0,0 +1,6 @@
+import ConnectionPageComponent from "@/components/Connection";
+import GuildComponent from "@/components/Dashboard/Guilds/Guild";
+
+export default function ConnectionPage() {
+    return <ConnectionPageComponent />
+}
