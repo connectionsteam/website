3c47030d7a2817187fe9d4f6ca1d26774c3d7a8d
Spyei <caio.salamao1234@gmail.com>
2024-07-10 22:42:14 -0300

    fix: default disabled button

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from "react";
 
-export default function DefaultButton({ children, className, onClick }: { children: ReactNode, className?: string, onClick?: () => void }) {
+interface Props {
+    children: ReactNode;
+    className?: string;
+    onClick?: () => void;
+    disabled?: boolean;
+}
+
+export default function DefaultButton({ children, className, onClick, disabled }: Props) {
     return (
-        <div className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full">
-            <button onClick={onClick} className={`flex items-center justify-center gap-2 ph-full w-full rounded-lg bg-neutral-800 hover:bg-transparent transition ${className}`}>
+        <div aria-disabled={disabled} className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full">
+            <button
+                disabled={disabled || false}
+                onClick={onClick}
+                className={`${className} flex items-center justify-center gap-2 ph-full w-full rounded-lg bg-neutral-800 hover:bg-transparent transition disabled:hover:bg-neutral-800`}
+            >
                 {children}
             </button>
         </div>
