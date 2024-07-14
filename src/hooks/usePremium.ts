2366c74380b8e7fc5a82bf3781c9f9474a0f188d
Spyei <caio.salamao1234@gmail.com>
2024-07-14 15:00:55 -0300

    fix: use premium undefined guild

--- src/hooks/usePremium.ts
+++ src/hooks/usePremium.ts
@@ -1,33 +1,27 @@
+import { useState, useEffect } from "react";
 import { GuildPayload, Premium as PremiumStructure, PremiumType } from "@/types";
-import { useState } from "react";
 
-export default function usePremium(guild: GuildPayload) {
-    const [premium, setPremium] = useState<PremiumStructure>(() => {
-        if (!guild.premium) {
-            return {
-                isPremium: false,
-                maxMods: 5,
-                maxConnections: 5,
-                maxThreads: 0,
-                premiumType: PremiumType.None
-            };
-        }
+export default function usePremium(guild: GuildPayload | undefined) {
+    const [premium, setPremium] = useState<PremiumStructure>();
 
-        const premiumType = guild.premium.type;
-        const maxMods = 10;
+    useEffect(() => {
+        if (!guild) return;
+
+        const premiumType = guild.premium?.type || PremiumType.None;
+        const maxMods = premiumType === PremiumType.None ? 5 : 10;
         const maxConnections = premiumType === PremiumType.Normal ? 25
             : premiumType === PremiumType.Vip ? 50 : 5;
         const maxThreads = premiumType === PremiumType.Normal ? 5
             : premiumType === PremiumType.Vip ? 15 : 0;
 
-        return {
-            isPremium: true,
+        setPremium({
+            isPremium: premiumType !== PremiumType.None,
             maxMods,
             maxConnections,
             maxThreads,
             premiumType
-        };
-    });
+        });
+    }, [guild]);
 
     return {
         premium,
