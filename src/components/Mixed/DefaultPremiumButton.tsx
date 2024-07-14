9f0dd8c3d9cdc8badd4f8942fb7e3bcc537396d3
Spyei <caio.salamao1234@gmail.com>
2024-07-14 18:31:15 -0300

    feat: adding link possiblity in premium button

--- src/components/Mixed/DefaultPremiumButton.tsx
+++ src/components/Mixed/DefaultPremiumButton.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { HiSparkles } from "react-icons/hi";
 
@@ -7,23 +8,42 @@ interface Props {
     className?: string;
     onClick?: () => void;
     loading?: boolean;
+    link?: boolean;
+    href?: string;
 }
 
-export default function DefaultPremiumButton({ disabled, text, className, onClick, loading }: Props) {
+export default function DefaultPremiumButton({ disabled, text, className, onClick, loading, link, href }: Props) {
     return (
         <div className="p-0.5 rounded-lg bg-gradient-to-r from-amber-400 to-orange-500 w-full">
-            <button
-                onClick={onClick}
-                disabled={disabled}
-                className={`p-3 rounded-lg bg-neutral-800 hover:bg-transparent transition w-full h-full disabled:text-neutral-300 disabled:hover:bg-neutral-800 flex gap-2 items-center justify-center ${className} group`}
-            >
-                {loading ? (
-                    <AiOutlineLoading3Quarters className="animate-spin" size={23} />
-                ) : (
-                    <HiSparkles className="fill-yellow-500 group-hover:fill-white transition group-disabled:group-hover:fill-yellow-500" size={23} />
-                )}
-                <span>{text}</span>
-            </button>
+            {link ? (
+                <Link href={href || ""} className="p-3 rounded-lg bg-neutral-800 hover:bg-transparent transition w-full h-full 
+                    isabled:text-neutral-300 disabled:hover:bg-neutral-800 flex gap-2 items-center 
+                    justify-center ${className} group
+                ">
+                    {loading ? (
+                        <AiOutlineLoading3Quarters className="animate-spin" size={23} />
+                    ) : (
+                        <HiSparkles className="fill-yellow-500 group-hover:fill-white transition group-disabled:group-hover:fill-yellow-500" size={23} />
+                    )}
+                    <span>{text}</span>
+                </Link>
+            ) : (
+                <button
+                    onClick={onClick}
+                    disabled={disabled}
+                    className={`p-3 rounded-lg bg-neutral-800 hover:bg-transparent transition w-full h-full 
+                    isabled:text-neutral-300 disabled:hover:bg-neutral-800 flex gap-2 items-center 
+                    justify-center ${className} group
+                `}
+                >
+                    {loading ? (
+                        <AiOutlineLoading3Quarters className="animate-spin" size={23} />
+                    ) : (
+                        <HiSparkles className="fill-yellow-500 group-hover:fill-white transition group-disabled:group-hover:fill-yellow-500" size={23} />
+                    )}
+                    <span>{text}</span>
+                </button>
+            )}
         </div>
     );
 }
