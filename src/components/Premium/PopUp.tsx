97c02f2a49883df6ff607f43eee5cc0c2d695cb7
Spyei <caio.salamao1234@gmail.com>
2024-07-14 22:42:25 -0300

    fix: sonner in premium pop up

--- src/components/Premium/PopUp.tsx
+++ src/components/Premium/PopUp.tsx
@@ -1,23 +1,41 @@
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, UseDisclosureProps } from "@nextui-org/modal";
+import DefaultPremiumButton from "../Mixed/DefaultPremiumButton";
+import Sonner from "../Mixed/Sonner";
+import { useEffect, useState } from "react";
 
 interface Props extends UseDisclosureProps {
     text: string;
+    limitText?: string;
+    limit?: boolean;
+    sonner?: boolean;
 }
 
-export default function PremiumPopUp({ isOpen, onChange, text }: Props) {
-    return (
+export default function PremiumPopUp({ isOpen, onChange, text, limit, limitText, sonner }: Props) {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        if (sonner) {
+            setActive(true);
+        }
+    }, [sonner]);
+
+    return (limit && limitText && sonner) ? (
+        <Sonner active={active} text={limitText} setActive={setActive} />
+    ) : (
         <Modal classNames={{
             closeButton: "transition hover:bg-neutral-700",
             wrapper: "overflow-y-hidden",
             base: "max-h-screen overflow-y-auto",
-        }} isOpen={isOpen} onOpenChange={onChange}>
+        }} isOpen={isOpen} onOpenChange={onChange} >
             <ModalContent className="bg-neutral-800 text-white">
-                <ModalHeader className="flex flex-col gap-1 bg-neutral-800">Premium</ModalHeader>
+                <ModalHeader className="flex flex-col gap-1 bg-neutral-800">
+                    Comprar Premium
+                </ModalHeader>
                 <ModalBody className="flex gap-2">
-                    <div>Precisa do premium amigao</div>
+                    <div>{text}</div>
                 </ModalBody>
                 <ModalFooter className="flex w-full justify-center">
-                    <div>footer</div>
+                    <DefaultPremiumButton text="Adquirir Premium" link href="/premium" />
                 </ModalFooter>
             </ModalContent>
         </Modal>
