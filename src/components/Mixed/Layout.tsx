08ea5f70480e0387f95d385ee3e840a4e7241060
Spyei <caio.salamao1234@gmail.com>
2024-07-15 11:16:39 -0300

    fix: default layout margin

--- src/components/Mixed/Layout.tsx
+++ src/components/Mixed/Layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function DefaultLayout({ children, className }: { children: ReactNode, className?: string }) {
     return (
         <div className="flex items-center justify-center text-white">
-            <div className={`${className || ""} flex w-full max-w-[1100px] items-start flex-col gap-4 z-10 tablet:px-3 mt-28`}>
+            <div className={`${className} flex w-full max-w-[1100px] items-start flex-col gap-4 z-10 tablet:px-3 mt-24`}>
                 {children}
             </div>
         </div>
