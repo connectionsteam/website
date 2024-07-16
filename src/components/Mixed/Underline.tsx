4fde01f7a8ddc23e77dce1d0a9ec2e638d6f7d5f
Spyei <caio.salamao1234@gmail.com>
2024-07-16 15:46:29 -0300

    feat: underline premium hover

--- src/components/Mixed/Underline.tsx
+++ src/components/Mixed/Underline.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from "react";
 
-export default function Underline({ children }: { children: ReactNode }) {
+interface Props {
+    children: ReactNode;
+    premium?: boolean;
+}
+
+export default function Underline({ children, premium }: Props) {
     return (
-        <span className="bg-left-bottom bg-gradient-to-r from-fuchsia-500 to-indigo-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"> 
+        <span className={`bg-left-bottom bg-gradient-to-r 
+            ${premium ? `from-yellow-500 to-amber-500` : `from-fuchsia-500 to-indigo-500`}
+        bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all 
+        duration-500 ease-out`}>
             {children}
         </span>
     )
