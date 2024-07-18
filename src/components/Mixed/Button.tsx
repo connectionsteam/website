c65ac021b1fb86561ca63095b16921d88970799a
Spyei <caio.salamao1234@gmail.com>
2024-07-17 22:27:13 -0300

    feat: default button link

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/utils/cn";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 interface Props {
@@ -5,18 +7,33 @@ interface Props {
     className?: string;
     onClick?: () => void;
     disabled?: boolean;
+    href?: string;
 }
 
-export default function DefaultButton({ children, className, onClick, disabled }: Props) {
+export default function DefaultButton({ children, className, onClick, disabled, href }: Props) {
+    const sharedClasses = cn(
+        "flex gap-2 h-full items-center justify-center w-full rounded-lg bg-neutral-800 hover:bg-transparent transition disabled:hover:bg-neutral-800 text-center",
+        className
+    );
+
     return (
-        <div aria-disabled={disabled} className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full">
-            <button
-                disabled={disabled || false}
-                onClick={onClick}
-                className={`${className} flex gap-2 ph-full w-full rounded-lg bg-neutral-800 hover:bg-transparent transition disabled:hover:bg-neutral-800`}
-            >
-                {children}
-            </button>
+        <div className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg">
+            {href ? (
+                <Link
+                    href={href}
+                    className={sharedClasses}
+                >
+                    {children}
+                </Link>
+            ) : (
+                <button
+                    disabled={disabled || false}
+                    onClick={onClick}
+                    className={sharedClasses}
+                >
+                    {children}
+                </button>
+            )}
         </div>
     );
 }
