586edd1753531a4a38314a643009e80c37cabeaf
Spyei <caio.salamao1234@gmail.com>
2024-07-18 23:08:55 -0300

    chore: button width

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -8,16 +8,17 @@ interface Props {
     onClick?: () => void;
     disabled?: boolean;
     href?: string;
+    divclass?: string;
 }
 
-export default function DefaultButton({ children, className, onClick, disabled, href }: Props) {
+export default function DefaultButton({ children, className, onClick, disabled, href, divclass }: Props) {
     const sharedClasses = cn(
-        "flex gap-2 h-full items-center justify-center w-full rounded-lg bg-neutral-800 hover:bg-transparent transition disabled:hover:bg-neutral-800 text-center",
+        "flex gap-2 h-full w-full items-center justify-center w-full rounded-lg bg-neutral-800 hover:bg-transparent transition disabled:hover:bg-neutral-800 text-center",
         className
     );
 
     return (
-        <div className="p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg">
+        <div className={cn("p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full", divclass)}>
             {href ? (
                 <Link
                     href={href}
