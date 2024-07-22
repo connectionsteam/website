e6b8cc1768a1945b51b3ed8d42f25a41c942b8fa
Spyei <caio.salamao1234@gmail.com>
2024-07-22 13:28:25 -0300

    feat: better guild cases

--- src/components/Dashboard/Guilds/Guild/Cases/Card.tsx
+++ src/components/Dashboard/Guilds/Guild/Cases/Card.tsx
@@ -1,45 +1,80 @@
 import Avatar from "@/components/Mixed/Avatar";
 import { useLanguage } from "@/hooks/useLanguage";
-import { AnyCase, CaseTypes, DiscordMember } from "@/types";
+import { AnyCase, CaseTypes } from "@/types";
 import moment from "moment";
+import { IoIosArrowDown } from "react-icons/io";
 
 interface Props {
     caseItem: AnyCase;
-    moderator: DiscordMember | undefined;
-    target: DiscordMember | undefined;
+    index: number;
+    hovering: boolean;
+    loading: boolean;
 }
 
-export default function CaseCard({ caseItem, moderator, target }: Props) {
+export default function CaseCard({ caseItem, index, hovering, loading }: Props) {
     const l = useLanguage();
 
-    return (
-        <div key={caseItem.id} className="flex flex-col gap-1 items-start w-full rounded-lg bg-neutral-900 p-4">
-            <div className="flex gap-2 items-center mobile:flex-col mobile:items-start">
-                {target ? (
-                    <div className="flex gap-2 items-center">
-                        <Avatar className="w-6 h-6" src={`https://cdn.discordapp.com/avatars/${target.user.id}/${target.user.avatar}`} />
-                        <span className="font-bold">{target.user.username}</span>
-                    </div>
-                ) : (
-                    <div className="font-bold">{caseItem.targetId}</div>
-                )}
-                <span>foi {caseItem.type === CaseTypes.Ban ? l.dashboard.guilds.cases.ban : l.dashboard.guilds.cases.mute} {l.dashboard.guilds.cases.by}</span>
-                {moderator ? (
-                    <div className="flex gap-2 items-center">
-                        <Avatar className="w-6 h-6" src={`https://cdn.discordapp.com/avatars/${moderator.user.id}/${moderator.user.avatar}`} />
-                        <span className="font-bold">{moderator.user.username}</span>
-                    </div>
-                ) : (
-                    <div className="font-bold">{caseItem.moderatorId}</div>
-                )}
+    return loading ? (
+        <div className="p-3 w-full bg-neutral-900/50 rounded-lg flex flex-col gap-1">
+            <div className="font-bold w-full rounded-lg flex items-center ">
+                <span className="flex flex-grow">{l.dashboard.guilds.cases.case} {caseItem.id}</span>
+                <IoIosArrowDown />
             </div>
-            {caseItem.reason ? (
-                <div className="flex gap-2 items-center">
-                    <span className="font-bold">{l.dashboard.guilds.cases.reason}:</span>
-                    <span className="text-neutral-300">{caseItem.reason}</span>
-                </div>
-            ) : null}
-            <span className="text-neutral-300 text-sm">{moment(caseItem.createdTimestamp).fromNow()}</span>
+            <div className="flex flex-col gap-2 items-start w-full rounded-lg p-2 animate-pulse">
+                <div className="bg-neutral-700 h-4 rounded-full w-72 mobile:w-full"></div>
+                <div className="bg-neutral-700 h-4 rounded-full w-80 mobile:w-1/2"></div>
+                <div className="bg-neutral-700 h-4 rounded-full w-80 mobile:w-1/3"></div>
+                <div className="bg-neutral-700 h-4 rounded-full w-80 mobile:w-full"></div>
+                <div className="bg-neutral-700 h-4 rounded-full w-32 mobile:w-24"></div>
+            </div>
         </div>
-    )
+    ) : hovering ? (
+        <div className="p-3 w-full bg-neutral-900/50 rounded-lg flex flex-col gap-1">
+            <div className="font-bold w-full rounded-lg flex items-center ">
+                <span className="flex flex-grow">{l.dashboard.guilds.cases.case} {caseItem.id}</span>
+                <IoIosArrowDown />
+            </div>
+            <div
+                key={caseItem.id}
+                className="flex flex-col gap-2 items-start w-full rounded-lg p-2"
+            >
+                <div className="flex gap-2 items-center mobile:flex-col mobile:items-start">
+                    <div className="flex gap-2 items-center">
+                        <Avatar
+                            className="w-6 h-6"
+                            src={`https://cdn.discordapp.com/avatars/${caseItem.targetId}/${caseItem.target.avatar}`}
+                        />
+                        <span className="font-bold">{caseItem.target.username}</span>
+                    </div>
+                    <span>
+                        {l.dashboard.guilds.cases.was} {caseItem.type === CaseTypes.Ban
+                            ? l.dashboard.guilds.cases.ban
+                            : l.dashboard.guilds.cases.mute}{" "}
+                        {l.dashboard.guilds.cases.by}
+                    </span>
+                    <div className="flex gap-2 items-center">
+                        <Avatar
+                            className="w-6 h-6"
+                            src={`https://cdn.discordapp.com/avatars/${caseItem.moderatorId}/${caseItem.moderator.avatar}`}
+                        />
+                        <span className="font-bold">{caseItem.moderator.username}</span>
+                    </div>
+                </div>
+                {caseItem.reason ? (
+                    <div className="flex gap-2 items-center mobile:flex-col mobile:items-start mobile:gap-1">
+                        <span className="font-bold">{l.dashboard.guilds.cases.reason}:</span>
+                        <span className="text-neutral-300">{caseItem.reason}</span>
+                    </div>
+                ) : null}
+                <span className="text-neutral-300 text-sm">
+                    {moment(caseItem.createdTimestamp).fromNow()}
+                </span>
+            </div>
+        </div>
+    ) : (
+        <div className="font-bold p-3 w-full bg-neutral-900/50 rounded-lg flex items-center ">
+            <span className="flex flex-grow">{l.dashboard.guilds.cases.case} {caseItem.id}</span>
+            <IoIosArrowDown />
+        </div>
+    );
 }
--- src/components/Dashboard/Guilds/Guild/Cases/index.tsx
+++ src/components/Dashboard/Guilds/Guild/Cases/index.tsx
@@ -1,15 +1,33 @@
-import { DiscordMember, GuildPayload } from "@/types";
+import { AnyCase, GuildPayload } from "@/types";
 import CaseCard from "./Card";
 import { useLanguage } from "@/hooks/useLanguage";
+import { useState } from "react";
+import { api } from "@/utils/api";
 
 interface Props {
     guild: GuildPayload;
-    members: DiscordMember[];
 }
 
-export default function Cases({ guild, members }: Props) {
+export default function Cases({ guild }: Props) {
     const l = useLanguage();
-    
+    const [cases, setCases] = useState<AnyCase[]>([]);
+    const [loading, setLoading] = useState<string | null>(null);
+
+    const fetchCase = async (caseId: string) => {
+        if (cases.find((c) => c.id === caseId)) return;
+
+        setLoading(caseId);
+
+        const { data } = await api.get(`/guilds/${guild.id}/cases/${caseId}`);
+        
+        setCases((prevCases) => [
+            ...prevCases.filter((caseItem) => caseItem.id !== caseId),
+            data,
+        ]);
+
+        setLoading(null);
+    };
+
     return (
         <div className="flex flex-col gap-4 rounded-lg bg-neutral-800 p-6">
             <div className="flex flex-col">
@@ -19,11 +37,21 @@ export default function Cases({ guild, members }: Props) {
                 </span>
                 <div className="w-full">
                     <div className="flex flex-col gap-4">
-                        {guild.cases.map((caseItem) => {
-                            const moderator = members.find((m) => m.user.id === caseItem.moderatorId);
-                            const target = members.find((m) => m.user.id === caseItem.targetId);
+                        {guild.cases.map((caseItem, index) => {
+                            const caseDetail = cases.find((c) => c.id === caseItem.id);
 
-                            return <CaseCard key={caseItem.id} caseItem={caseItem} moderator={moderator} target={target} />;
+                            return (
+                                <div
+                                    onClick={() => fetchCase(caseItem.id)}
+                                    key={caseItem.id}>
+                                    <CaseCard
+                                        loading={loading === caseItem.id}
+                                        hovering={caseItem.id === caseDetail?.id}
+                                        caseItem={caseDetail || caseItem}
+                                        index={index}
+                                    />
+                                </div>
+                            );
                         })}
                     </div>
                 </div>
