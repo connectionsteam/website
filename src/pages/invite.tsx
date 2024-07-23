859fded182952767020c33c0d6f4df116509c24d
Spyei <caio.salamao1234@gmail.com>
2024-07-23 12:11:45 -0300

    feat: invite page

---
+++ src/pages/invite.tsx
@@ -0,0 +1,12 @@
+import { useIsClient } from "@/contexts/Client";
+import { useEffect } from "react";
+
+export default function Invite() {
+    const isClient = useIsClient();
+
+    if (!isClient) return null;
+
+    useEffect(() => {
+        window.location.href = "https://discord.com/oauth2/authorize?client_id=1243234162077470802";
+    }, []);
+}
