149085178c18c10f671599d75778282cc4fd4482
Spyei <caio.salamao1234@gmail.com>
2024-07-26 15:31:18 -0300

    feat: debounce function

---
+++ src/utils/debounce.ts
@@ -0,0 +1,8 @@
+export default function debounce<T extends (...args: any[]) => void>(func: T, wait: number): (...args: Parameters<T>) => void {
+    let timeout: NodeJS.Timeout;
+
+    return (...args: Parameters<T>) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(null, args), wait);
+    };
+}
