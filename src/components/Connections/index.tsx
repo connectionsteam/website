b5624435653236ee021209978f32ed346d419801
Spyei <caio.salamao1234@gmail.com>
2024-07-26 15:43:12 -0300

    feat: implementing connections filters

--- src/components/Connections/index.tsx
+++ src/components/Connections/index.tsx
@@ -2,17 +2,19 @@ import { useEffect, useState, useRef } from "react";
 import DefaultLayout from "../Mixed/Layout";
 import { ConnectionsPageStructure } from "@/types";
 import { api } from "@/utils/api";
-import { Input } from "@nextui-org/input";
 import { useLanguage } from "@/hooks/useLanguage";
 import { LuGrid, LuList } from "react-icons/lu";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import ConnectionsPageCard from "./Connection";
 import { useIsClient } from "@/contexts/Client";
 import { RiHashtag } from "react-icons/ri";
-import { IoFilter } from "react-icons/io5";
 import { Modal, useDisclosure } from "@nextui-org/modal";
-import Filters from "./Filters";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import SearchConnection from "./Search";
+import Link from "next/link";
+import { IoFilter } from "react-icons/io5";
+import Filters from "./Filters";
+import DeskConnectionsFilters from "./DeskFilters";
 
 export default function ConnectionsPageComponent() {
     const l = useLanguage();
@@ -23,16 +25,24 @@ export default function ConnectionsPageComponent() {
     const [page, setPage] = useState(0);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    const [filters, setFilters] = useState({
-        tag: "",
-        sort: "",
-        query: "",
-    });
+    const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+    const [queryInput, setQueryInput] = useState("");
     const [options, setOptions] = useState(() => {
         const savedLayout = localStorage.getItem("layout");
         return savedLayout ? { layout: savedLayout } : { layout: "grid" };
     });
+    const [filters, setFilters] = useState({
+        tag: "",
+        sort: "",
+        query: "",
+        search: queryInput === "" ? true : false,
+    });
+    const [submited, setSubmited] = useState(false);
+
+    const queryParams = new URLSearchParams();
+    if (filters.tag !== "") queryParams.set("tag", filters.tag.toLowerCase());
+    if (filters.query !== "" && filters.search) queryParams.set("query", filters.query.toLowerCase());
+    if (filters.sort !== "") queryParams.set("sort", filters.sort.toLowerCase());
 
     const observer = useRef<IntersectionObserver | null>(null);
     const lastConnectionElementRef = useRef<HTMLDivElement | null>(null);
@@ -47,9 +57,9 @@ export default function ConnectionsPageComponent() {
 
     const fetchConnections = async (page = 0, append = false) => {
         setLoading(true);
-        const reqfilters = `${filters.tag !== "" ? `&tag=${filters.tag}` : ""}${filters.sort === "new" ? `&sort=new` : ""}`;
-        const { data } = await api.get(`/connections?query=${filters.query}${reqfilters}&start_at=${page * 10}&end_at=${(page + 1) * 10}`);
-        
+
+        const { data } = await api.get(`/connections?${queryParams}&start_at=${page * 10}&end_at=${(page + 1) * 10}`);
+
         if (data.length === 0) {
             setHasMore(false);
         } else {
@@ -61,7 +71,14 @@ export default function ConnectionsPageComponent() {
 
     useEffect(() => {
         fetchConnections();
-    }, [filters]);
+    }, []);
+
+    useEffect(() => {
+        if (!submited) return;
+
+        setSubmited(false);
+        fetchConnections();
+    }, [submited]);
 
     useEffect(() => {
         if (page > 0) {
@@ -72,7 +89,9 @@ export default function ConnectionsPageComponent() {
     useEffect(() => {
         if (loading || !hasMore) return;
 
-        if (observer.current) observer.current.disconnect();
+        if (observer.current) {
+            observer.current.disconnect();
+        }
 
         const options = {
             root: null,
@@ -81,9 +100,7 @@ export default function ConnectionsPageComponent() {
         };
 
         observer.current = new IntersectionObserver((entries) => {
-            if (entries[0].isIntersecting) {
-                setPage(prevPage => prevPage + 1);
-            }
+            if (entries[0].isIntersecting) setPage(prevPage => prevPage + 1);
         }, options);
 
         if (lastConnectionElementRef.current) {
@@ -104,25 +121,30 @@ export default function ConnectionsPageComponent() {
                     <h1 className="text-3xl font-bold">{l.connection.title}</h1>
                     <span className="text-neutral-300">{l.connection.description}</span>
                 </div>
-                <Input
-                    classNames={{
-                        inputWrapper: "rounded-lg bg-neutral-800 group-hover:bg-neutral-700",
-                    }}
-                    onChange={(e) => setFilters(prevFilters => ({ ...prevFilters, query: e.target.value }))}
-                    type="string"
-                    label={l.connection.search}
-                />
+                <div className="w-full relative h-full">
+                    {connections.length > 0 && (
+                        <SearchConnection
+                            queryInput={queryInput}
+                            setSubmited={setSubmited}
+                            query={queryInput}
+                            setQuery={setQueryInput}
+                            filters={filters}
+                            setFilters={setFilters}
+                            actualConnections={connections}
+                        />
+                    )}
+                </div>
                 <div className="w-full flex items-center justify-center gap-2 h-full">
                     <div className="flex gap-1 overflow-x-auto flex-grow">
                         {l.connection.tags.map((tag, index) => (
-                            <button
-                                onClick={() => setFilters({ ...filters, tag: tag })}
+                            <Link
+                                href={`/connections/search?tag=${tag}`}
                                 key={index}
                                 className="p-1 px-2 flex gap-2 items-center bg-neutral-800 rounded-lg"
                             >
                                 <RiHashtag fill="#d946ef" />
                                 <span className="whitespace-nowrap">{tag}</span>
-                            </button>
+                            </Link>
                         ))}
                     </div>
                     <div className="p-1 flex gap-2 items-center bg-neutral-800 rounded-lg relative h-full mobile:hidden">
@@ -143,7 +165,7 @@ export default function ConnectionsPageComponent() {
                             <LuGrid />
                         </button>
                     </div>
-                    <div className="p-1 flex gap-2 items-center bg-neutral-800 rounded-lg h-full mobile:hidden">
+                    <div className="p-1 flex gap-2 items-center bg-neutral-800 rounded-lg h-full tabletdesk:hidden">
                         <button
                             onClick={onOpen}
                             className="p-2 transition hover:bg-neutral-700 rounded-lg relative">
@@ -157,40 +179,42 @@ export default function ConnectionsPageComponent() {
                             wrapper: "overflow-y-hidden",
                             base: "max-h-screen overflow-y-auto",
                         }} isOpen={isOpen} onOpenChange={onOpenChange}>
-                            <Filters filters={filters} setFilters={setFilters} />
+                            <Filters
+                                closeForm={onClose}
+                                setSubmited={setSubmited}
+                                filters={filters}
+                                setFilters={setFilters}
+                            />
                         </Modal>
                     </div>
                 </div>
-                <div className="flex gap-4 w-full">
+                <div className="flex gap-4 w-full items-start">
                     <div className="flex w-full">
-                        <AnimatePresence mode="wait">
-                            <motion.div
-                                key={options.layout}
-                                className={options.layout === "grid" ?
-                                    "grid grid-cols-2 gap-3 w-full mobile:grid-cols-1"
-                                    : "flex flex-col gap-2 w-full"
-                                }
-                            >
-                                {connections.map((connection, index) => (
-                                    <ConnectionsPageCard
-                                        connections={connections}
-                                        layout={options.layout}
-                                        query={filters.query}
-                                        key={index}
-                                        connection={connection}
-                                        index={index}
-                                        ref={index === connections.length - 1 ? lastConnectionElementRef : null}
-                                    />
-                                ))}
-                            </motion.div>
-                        </AnimatePresence>
+                        <div className={options.layout === "grid" ?
+                            "grid grid-cols-2 gap-3 w-full mobile:grid-cols-1"
+                            : "flex flex-col gap-2 w-full"
+                        }
+                        >
+                            {connections.map((connection, index) => (
+                                <ConnectionsPageCard
+                                    connections={connections}
+                                    layout={options.layout}
+                                    key={index}
+                                    connection={connection}
+                                    index={index}
+                                    ref={index === connections.length - 1 ? lastConnectionElementRef : null}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                    <div className="flex px-2 items-center justify-center tablet:hidden w-80">
+                        <DeskConnectionsFilters
+                            setSubmited={setSubmited}
+                            filters={filters}
+                            setFilters={setFilters}
+                        />
                     </div>
                 </div>
-                {connections.length <= 0 && (
-                    <div className="w-full flex items-center text-center justify-center">
-                        <span>{l.connection.noConnections}</span>
-                    </div>
-                )}
                 {loading && (
                     <div className="w-full flex items-center text-center justify-center mt-2">
                         <AiOutlineLoading3Quarters className="animate-spin" size={30} />
