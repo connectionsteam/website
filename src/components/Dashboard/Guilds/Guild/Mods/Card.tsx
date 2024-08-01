e4b1d1d9f4e2c2bc4a28db63bcf12c97550936e4
Spyei <caio.salamao1234@gmail.com>
2024-08-01 10:46:00 -0300

    fix: moderator remove and add animation

--- src/components/Dashboard/Guilds/Guild/Mods/Card.tsx
+++ src/components/Dashboard/Guilds/Guild/Mods/Card.tsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import RemoveGuildMod from "./Menu";
 import Avatar from "@/components/Mixed/Avatar";
 import { GuildPayload, ModPermType } from "@/types";
@@ -20,42 +20,41 @@ export default function GuildModCard({ index, mod, handleRemoveMod, menu, setMen
     const owner = Object.entries(guild.mods)
         .find(([id, mod]) => id === user?.id && mod.type === ModPermType.PhysicalOwner);
 
-    return (
-        <AnimatePresence key={index}>
-            {menu.removing !== mod.id && (
-                <motion.div
-                    initial={{ opacity: 0, x: -20 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: -20 }}
-                    onMouseEnter={() => setMenu({ ...menu, hover: mod.id })}
-                    onMouseLeave={() => setMenu({ ...menu, hover: "" })}
-                    key={index}
-                    className="w-full relative"
-                >
-                    {(owner && mod.id !== user?.id) && (
-                        <>
-                            <div className="w-full relative tabletdesk:invisible">
-                                <RemoveGuildMod
-                                    mod={{ avatar: mod.avatar, id: mod.id, username: mod.username }}
-                                    open={true}
-                                    handleRemove={() => handleRemoveMod(mod.id)}
-                                />
-                            </div>
-                            <RemoveGuildMod
-                                mod={{ avatar: mod.avatar, id: mod.id, username: mod.username }}
-                                open={menu.hover === mod.id}
-                                handleRemove={() => handleRemoveMod(mod.id)}
-                            />
-                        </>
-                    )}
-                    <div className="flex gap-3 items-center rounded-lg p-2 bg-neutral-900/50 w-full">
-                        <div className="h-12 w-12">
-                            <Avatar className="w-12 h-12" src={`https://cdn.discordapp.com/avatars/${mod.id}/${mod.avatar}.png`} />
-                        </div>
-                        <span className="font-semibold text-lg">{mod.username}</span>
+    return menu.removing !== mod.id && (
+        <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -20 }}
+            onMouseEnter={() => setMenu({ ...menu, hover: mod.id })}
+            onMouseLeave={() => setMenu({ ...menu, hover: "" })}
+            key={index}
+            className="w-full relative"
+        >
+            {(owner && mod.id !== user?.id) && (
+                <>
+                    <div className="w-full relative tabletdesk:invisible">
+                        <RemoveGuildMod
+                            mod={{ avatar: mod.avatar, id: mod.id, username: mod.username }}
+                            open={true}
+                            handleRemove={() => handleRemoveMod(mod.id)}
+                        />
                     </div>
-                </motion.div>
+                    <RemoveGuildMod
+                        mod={{ avatar: mod.avatar, id: mod.id, username: mod.username }}
+                        open={menu.hover === mod.id}
+                        handleRemove={() => handleRemoveMod(mod.id)}
+                    />
+                </>
             )}
-        </AnimatePresence>
+            <div className="flex gap-3 items-center rounded-lg p-2 bg-neutral-900/50 w-full">
+                <div className="h-12 w-12">
+                    <Avatar
+                        className="w-12 h-12"
+                        src={`https://cdn.discordapp.com/avatars/${mod.id}/${mod.avatar}.png`}
+                    />
+                </div>
+                <span className="font-semibold text-lg">{mod.username}</span>
+            </div>
+        </motion.div>
     );
 }
