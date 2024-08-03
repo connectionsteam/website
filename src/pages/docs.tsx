056ac333eaada5feea929fcb02d3b7befd0db356
Spyei <caio.salamao1234@gmail.com>
2024-08-03 15:14:57 -0300

    feat: docs page redirect

---
+++ src/pages/docs.tsx
@@ -0,0 +1,7 @@
+import { useEffect } from "react";
+
+export default function Docs() {
+    useEffect(() => {
+        window.location.href = "https://connections-b7efc2e5.mintlify.app";
+    }, []);
+}
