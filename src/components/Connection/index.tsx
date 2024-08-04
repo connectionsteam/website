06b30c2b0b1cc5bb9f0e44e88a0907bff4eade61
Spyei <caio.salamao1234@gmail.com>
2024-08-03 23:10:55 -0300

    chore: trying to show connection meta

--- src/components/Connection/index.tsx
+++ src/components/Connection/index.tsx
@@ -13,6 +13,7 @@ import ConnectConnection from "./Connect";
 import Link from "next/link";
 import { UserContext } from "../../contexts/User";
 import ConnectionPageSkeleton from "./Skeleton";
+import Head from "next/head";
 
 export default function ConnectionPageComponent() {
     const router = useRouter();
@@ -62,76 +63,84 @@ export default function ConnectionPageComponent() {
     }, [connection]);
 
     return connection && recommendedConnections ? (
-        <DefaultLayout>
-            <div className="bg-neutral-800 rounded-lg w-full flex flex-col gap-4 p-6">
-                <div className="flex w-full tablet:flex-col tablet:gap-4">
-                    <div className="flex gap-4 items-center flex-grow mobile:flex-col">
-                        {connection.icon && (
-                            <Avatar src={connection.icon} className="w-24 h-24" />
-                        )}
-                        <div className="flex flex-col gap-2 items-start mobile:items-center">
-                            <span className="font-bold text-2xl">{connection.name}</span>
-                            {connection.description && (
-                                <div className="flex items-start text-start w-full break-words rounded-lg mobile:text-center">
-                                    <span>{connection.description}</span>
-                                </div>
+        <>
+            <Head>
+                <title>{connection.name || "Connection"}</title>
+                <meta name="og:image" content={connection.icon} />
+                <meta name="og:title" content={connection.name} />
+                <meta name="theme-color" content="#D946EF" />
+            </Head>
+            <DefaultLayout>
+                <div className="bg-neutral-800 rounded-lg w-full flex flex-col gap-4 p-6">
+                    <div className="flex w-full tablet:flex-col tablet:gap-4">
+                        <div className="flex gap-4 items-center flex-grow mobile:flex-col">
+                            {connection.icon && (
+                                <Avatar src={connection.icon} className="w-24 h-24" />
                             )}
-                            <div className="text-neutral-300 px-2 bg-neutral-700 rounded-lg flex items-center justify-center transition">
-                                <span>{voteProps.votes}</span>
-                                <MdOutlineKeyboardArrowUp size={20} />
+                            <div className="flex flex-col gap-2 items-start mobile:items-center">
+                                <span className="font-bold text-2xl">{connection.name}</span>
+                                {connection.description && (
+                                    <div className="flex items-start text-start w-full break-words rounded-lg mobile:text-center">
+                                        <span>{connection.description}</span>
+                                    </div>
+                                )}
+                                <div className="text-neutral-300 px-2 bg-neutral-700 rounded-lg flex items-center justify-center transition">
+                                    <span>{voteProps.votes}</span>
+                                    <MdOutlineKeyboardArrowUp size={20} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex gap-3 min-w-[450px] justify-start items-start tablet:min-w-full mobile:flex-col">
-                        <ConnectionsPageVoteComponent
-                            connection={connection}
-                            voteProps={voteProps}
-                            setVoteProps={setVoteProps}
-                        />
-                        <ConnectConnection connection={connection} />
-                    </div>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <span className="font-bold">{l.connection.creator}</span>
-                    <div className="w-8 h-8">
-                        <Avatar
-                            className="w-full h-full"
-                            src={`https://cdn.discordapp.com/avatars/${connection.creator.id}/${connection.creator.avatar}.png`}
-                        />
-                    </div>
-                    <span className="font-bold text-lg">{connection.creator.username}</span>
-                </div>
-                <div className="flex items-center w-full gap-2">
-                    {connection.tags.map((tag, index) => (
-                        <button key={index} className="p-1 px-2 flex gap-2 items-center bg-neutral-700 rounded-lg">
-                            <RiHashtag fill="#d946ef" />
-                            <span>{tag}</span>
-                        </button>
-                    ))}
-                </div>
-                <span className="text-sm text-neutral-300 flex gap-1">
-                    <span>{l.dashboard.guilds.threads.thread.created}</span>
-                    {new Date(connection.createdTimestamp).toLocaleString(l.language)}
-                </span>
-            </div>
-            <div className="flex flex-col gap-4 w-full">
-                <span className="font-bold text-2xl">{l.connection.recommended}</span>
-                <div className="grid grid-cols-2 gap-4 w-full tablet:grid-cols-1">
-                    {recommendedConnections.map((connection, index) => (
-                        <Link
-                            className="w-full h-full flex"
-                            href={`/connection/${connection.name}`} key={index}
-                        >
-                            <ConnectionsPageCard
-                                layout=""
-                                connections={[]}
+                        <div className="flex gap-3 min-w-[450px] justify-start items-start tablet:min-w-full mobile:flex-col">
+                            <ConnectionsPageVoteComponent
                                 connection={connection}
-                                index={index}
+                                voteProps={voteProps}
+                                setVoteProps={setVoteProps}
                             />
-                        </Link>
-                    ))}
+                            <ConnectConnection small={false} connection={connection} />
+                        </div>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <span className="font-bold">{l.connection.creator}</span>
+                        <div className="w-8 h-8">
+                            <Avatar
+                                className="w-full h-full"
+                                src={`https://cdn.discordapp.com/avatars/${connection.creator.id}/${connection.creator.avatar}.png`}
+                            />
+                        </div>
+                        <span className="font-bold text-lg">{connection.creator.username}</span>
+                    </div>
+                    <div className="flex items-center w-full gap-2">
+                        {connection.tags.map((tag, index) => (
+                            <button key={index} className="p-1 px-2 flex gap-2 items-center bg-neutral-700 rounded-lg">
+                                <RiHashtag fill="#d946ef" />
+                                <span>{tag}</span>
+                            </button>
+                        ))}
+                    </div>
+                    <span className="text-sm text-neutral-300 flex gap-1">
+                        <span>{l.dashboard.guilds.threads.thread.created}</span>
+                        {new Date(connection.createdTimestamp).toLocaleString(l.language)}
+                    </span>
                 </div>
-            </div>
-        </DefaultLayout>
+                <div className="flex flex-col gap-4 w-full">
+                    <span className="font-bold text-2xl">{l.connection.recommended}</span>
+                    <div className="grid grid-cols-2 gap-4 w-full tablet:grid-cols-1">
+                        {recommendedConnections.map((connection, index) => (
+                            <Link
+                                className="w-full h-full flex"
+                                href={`/connection/${connection.name}`} key={index}
+                            >
+                                <ConnectionsPageCard
+                                    layout=""
+                                    connections={[]}
+                                    connection={connection}
+                                    index={index}
+                                />
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </DefaultLayout>
+        </>
     ) : <ConnectionPageSkeleton />;
 }
