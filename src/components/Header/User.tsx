e5faec94193139fb851f0ef609bbd194d47b4347
Spyei <caio.salamao1234@gmail.com>
2024-08-14 21:41:53 -0300

    feat: notification bell header

--- src/components/Header/User.tsx
+++ src/components/Header/User.tsx
@@ -1,20 +1,23 @@
 import { UserContext } from "../../contexts/User";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
 import { useContext } from "react";
-import { LuLogOut } from "react-icons/lu";
+import { LuBell, LuLogOut } from "react-icons/lu";
 import Cookies from "js-cookie";
 import { useIsClient } from "../../contexts/Client";
 import axios from "axios";
 import Link from "next/link";
 import Avatar from "../Mixed/Avatar";
-import { LanguageContext } from "../../contexts/Language";
-import { languages } from "../../locale";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { useRouter } from "next/router";
+import { useLanguage } from "../../hooks/useLanguage";
+import { NotificationsContext } from "../../contexts/Notification";
 
 export default function AuthUser({ type, handleRecallDrawer }: { type: "mobile" | "desktop"; handleRecallDrawer: () => void; }) {
     const { user } = useContext(UserContext);
     const isClient = useIsClient();
-    const { language } = useContext(LanguageContext);
+    const { pathname } = useRouter();
+    const { notifications } = useContext(NotificationsContext);
+    const l = useLanguage();
 
     const handleLogin = () => {
         if (!isClient) return;
@@ -52,13 +55,13 @@ export default function AuthUser({ type, handleRecallDrawer }: { type: "mobile" 
                     <DropdownItem className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none" key="dash">
                         <Link onClick={handleRecallDrawer} href="/dashboard" className="flex items-center justify-start min-w-[140px] py-2 gap-3">
                             <MdOutlineSpaceDashboard />
-                            <span>{languages[language].home.header.menu.dashboard}</span>
+                            <span>{l.home.header.menu.dashboard}</span>
                         </Link>
                     </DropdownItem>
                     <DropdownItem className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none" key="exit">
                         <button onClick={handleLogout} className="flex items-center min-w-[140px] py-2 gap-3">
                             <LuLogOut />
-                            <span>{languages[language].home.header.menu.exit}</span>
+                            <span>{l.home.header.menu.exit}</span>
                         </button>
                     </DropdownItem>
                 </DropdownMenu>
@@ -68,34 +71,52 @@ export default function AuthUser({ type, handleRecallDrawer }: { type: "mobile" 
         </button>
     ) : (
         user ? (
-            <Dropdown className="bg-neutral-800 text-white rounded-lg outline-none">
-                <DropdownTrigger className="focus:bg-neutral-900">
-                    <button className="outline-none flex gap-2 justify-center items-center tablet:hidden p-2 transition hover:bg-neutral-900 rounded-lg">
-                        <span className="font-bold">{user.username}</span>
-                        <Avatar src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} />
-                    </button >
-                </DropdownTrigger >
-                <DropdownMenu className="bg-neutral-800 rounded-lg outline-none">
-                    <DropdownItem
-                        className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none"
-                        key="dash"
-                    >
-                        <Link href="/dashboard" className="flex items-center justify-start min-w-28 py-2 gap-3">
-                            <MdOutlineSpaceDashboard />
-                            <span>{languages[language].home.header.menu.dashboard}</span>
-                        </Link>
-                    </DropdownItem>
-                    <DropdownItem
-                        className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none"
-                        key="exit"
-                    >
-                        <button onClick={handleLogout} className="flex items-center min-w-28 py-2 gap-3">
-                            <LuLogOut />
-                            <span>{languages[language].home.header.menu.exit}</span>
-                        </button>
-                    </DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+            <div className="flex items-center gap-1">
+                <Link
+                    href="/notifications"
+                    className="hover:bg-neutral-900 rounded-lg transition p-3 flex 
+                        items-center justify-center relative"
+                >
+                    {(notifications?.length !== 0 && notifications) ? (
+                        <div className="absolute top-0 right-0 bg-red-500 rounded-full h-3.5 w-3.5 flex text-center items-center justify-center">
+                            <span className="text-xs">
+                                {notifications.length > 9
+                                    ? "9+"
+                                    : notifications.length}
+                            </span>
+                        </div>
+                    ) : null}
+                    <LuBell fill={pathname === "/notifications" ? "#fff" : ""} size={20} />
+                </Link>
+                <Dropdown className="bg-neutral-800 text-white rounded-lg outline-none">
+                    <DropdownTrigger className="focus:bg-neutral-900">
+                        <button className="outline-none flex gap-2 justify-center items-center tablet:hidden p-2 transition hover:bg-neutral-900 rounded-lg">
+                            <span className="font-bold">{user.username}</span>
+                            <Avatar src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} />
+                        </button >
+                    </DropdownTrigger >
+                    <DropdownMenu className="bg-neutral-800 rounded-lg outline-none">
+                        <DropdownItem
+                            className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none"
+                            key="dash"
+                        >
+                            <Link href="/dashboard" className="flex items-center justify-start min-w-28 py-2 gap-3">
+                                <MdOutlineSpaceDashboard />
+                                <span>{l.home.header.menu.dashboard}</span>
+                            </Link>
+                        </DropdownItem>
+                        <DropdownItem
+                            className="bg-neutral-800 rounded-lg cursor-pointer hover:bg-neutral-700 transition outline-none"
+                            key="exit"
+                        >
+                            <button onClick={handleLogout} className="flex items-center min-w-28 py-2 gap-3">
+                                <LuLogOut />
+                                <span>{l.home.header.menu.exit}</span>
+                            </button>
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </div>
         ) : <button onClick={handleLogin} className="tablet:hidden flex gap-2 justify-center items-center transition p-3 m-[2px] px-4 hover:bg-neutral-800 border-neutral-800 border-2 rounded-lg">
             Login
         </button>
