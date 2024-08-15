2a8a4bb3d12eaedeabba07d131ee0b79c0bf7056
Spyei <caio.salamao1234@gmail.com>
2024-08-15 17:37:19 -0300

    feat: teams list

---
+++ src/components/Dashboard/Teams/Team/index.tsx
@@ -0,0 +1,40 @@
+import { TeamPayload } from "../../../../types";
+import { motion } from "framer-motion";
+import { TeamState } from "..";
+import Avatar from "../../../Mixed/Avatar";
+import Link from "next/link";
+import DeleteTeam from "./Delete";
+
+interface Props {
+    team: TeamPayload;
+    index: number;
+    setTeamProps: (teamProps: TeamState) => void;
+    teamProps: TeamState;
+    handleDeleteTeam: () => void;
+}
+
+export default function TeamCard({ team, index, setTeamProps, teamProps, handleDeleteTeam }: Props) {
+    return (
+        <motion.div
+            key={team.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, x: -20 }}
+            transition={{ delay: index * 0.1, duration: 0.09 }}
+            onMouseEnter={() => setTeamProps({ ...teamProps, hover: team.id })}
+            onMouseLeave={() => setTeamProps({ ...teamProps, hover: null })}
+            className="w-full relative"
+        >
+            <DeleteTeam id={team.name} open={teamProps.hover === team.id} handleRemove={handleDeleteTeam} />
+            <Link
+                href={`/dashboard/team/${team.id}`}
+                className="flex items-center gap-2 p-3 rounded-lg bg-neutral-800 
+                hover:bg-neutral-700 transition relative w-full h-full">
+                <Avatar className="w-12 h-12" src={team.iconURL || ""} key={team.name} />
+                <div className="flex flex-col gap-1 text-start">
+                    <span className="font-bold text-lg">{team.name}</span>
+                </div>
+            </Link>
+        </motion.div>
+    )
+}
---
+++ src/components/Dashboard/Teams/index.tsx
@@ -0,0 +1,102 @@
+import { Input } from "@nextui-org/input";
+import { useLanguage } from "../../../hooks/useLanguage";
+import { ChangeEvent, useEffect, useState } from "react";
+import { MdOutlineSync } from "react-icons/md";
+import Head from "next/head";
+import { TeamPayload } from "../../../types";
+import { api } from "../../../utils/api";
+import { AnimatePresence } from "framer-motion";
+import TeamCard from "./Team";
+import DefaultButton from "../../Mixed/Button";
+import { LuPlusCircle } from "react-icons/lu";
+
+export interface TeamState {
+    team: TeamPayload | null;
+    hover: string | null;
+    removing: string | null;
+}
+
+export default function TeamsComponent() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [teams, setTeams] = useState<TeamPayload[] | null>(null);
+    const l = useLanguage();
+    const [teamProps, setteamProps] = useState<TeamState>({
+        team: null,
+        hover: null,
+        removing: null
+    });
+
+    const handleDeleteTeam = async () => {
+        setteamProps({ ...teamProps, removing: teamProps.hover });
+
+        await api.delete(`/teams/${teamProps.hover}`);
+
+        setTimeout(() => {
+            setTeams(teams!.filter(team => team.name !== teamProps.hover));
+
+            setteamProps({ ...teamProps, removing: null });
+        }, 500);
+    };
+
+    const handleChangeQuery = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const fetchTeams = async () => {
+        const { data } = await api.get("/users/@me/teams");
+
+        setTeams(data);
+    };
+
+    useEffect(() => {
+        fetchTeams();
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>{l.dashboard.teams.title}</title>
+            </Head>
+            <div className="flex w-full items-start flex-col gap-4 z-10 tablet:px-3">
+                <div className="flex flex-col gap-2">
+                    <h1 className="font-bold text-3xl">
+                        {l.dashboard.teams.title}
+                    </h1>
+                    <span className="text-neutral-300">
+                        {l.dashboard.teams.description}
+                    </span>
+                </div>
+                <div className="flex w-full h-full gap-1">
+                    <Input classNames={{
+                        inputWrapper: "rounded-lg bg-neutral-800 group-hover:bg-neutral-700",
+                    }} onChange={handleChangeQuery} type="string" label={l.dashboard.misc.filterTeams} />
+                    <DefaultButton divclass="w-fit" className="w-[52px]">
+                        <LuPlusCircle size={20} />
+                    </DefaultButton>
+                </div>
+                <div className="grid grid-cols-3 gap-3 w-full tablet:grid-cols-2 mobile:grid-cols-1">
+                    <AnimatePresence>
+                        {teams ? (
+                            teams
+                                .filter((team) =>
+                                    team.name.toLowerCase().includes(searchQuery.toLowerCase())
+                                    || team.name.includes(searchQuery)
+                                    || team.creatorId?.includes(searchQuery)
+                                )
+                                .map((team, index) => (
+                                    <TeamCard
+                                        handleDeleteTeam={handleDeleteTeam}
+                                        setTeamProps={setteamProps}
+                                        teamProps={teamProps}
+                                        team={team}
+                                        key={index}
+                                        index={index}
+                                    />
+                                ))
+                        ) : <div>a</div>}
+                    </AnimatePresence>
+                </div>
+            </div>
+        </>
+    )
+}
