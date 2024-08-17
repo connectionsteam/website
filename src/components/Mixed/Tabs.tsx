b0c31e51cf57ecfab2dcd242234de3cbaa86ac29
Spyei <caio.salamao1234@gmail.com>
2024-08-16 23:04:57 -0300

    feat: default tabs component

---
+++ src/components/Mixed/Tabs.tsx
@@ -0,0 +1,37 @@
+import { motion } from "framer-motion";
+
+interface Props {
+    tabs: { id: string; label: string }[];
+    cursor?: string;
+    activeTab: string;
+    setActiveTab: (activeTab: string) => void;
+}
+
+export default function DefaultTabs({ tabs, cursor, activeTab, setActiveTab }: Props) {
+    return (
+        <div className="flex space-x-1">
+            {tabs.map((tab) => (
+                <button
+                    key={tab.id}
+                    onClick={() => setActiveTab(tab.id)}
+                    className={`${activeTab === tab.id ? "" : "hover:text-white/70"} 
+                    relative rounded-lg px-3 py-1.5 text-white transition z-20`}
+                >
+                    {activeTab === tab.id && (
+                        <motion.span
+                            layoutId="bubble"
+                            className={`absolute inset-0 -z-10 ${!cursor ? "bg-neutral-700" : cursor}`}
+                            style={{ borderRadius: "8px" }}
+                            transition={{
+                                type: "spring",
+                                bounce: 0.3,
+                                duration: 0.5,
+                            }}
+                        />
+                    )}
+                    {tab.label}
+                </button>
+            ))}
+        </div>
+    );
+}
