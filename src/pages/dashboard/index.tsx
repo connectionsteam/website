905b47723e93806a745cff6d8c169a30938413b5
Spyei <caio.salamao1234@gmail.com>
2024-08-20 17:46:02 -0300

    chore: better dahboard tabs

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,12 +11,14 @@ import { useEffect, useState } from "react";
 import JoinPrivateConnectionModal from "../../components/Dashboard/Connection/JoinPrivateConnection";
 import { useLanguage } from "../../hooks/useLanguage";
 import TeamsComponent from "../../components/Dashboard/Teams";
+import DefaultTabs from "../../components/Mixed/Tabs";
 
 export default function DashboardPage({ query }: { query?: { name: string, code: string } }) {
     const l = useLanguage();
     const [connections, setConnections] = useState<ConnectionPayload[] | null>(null);
     const [guilds, setGuilds] = useState<GuildPayload[] | null>(null);
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [activeTab, setActiveTab] = useState("connections");
 
     const fetchGuilds = async () => {
         const { data } = await api.get("/users/@me/guilds");
@@ -40,43 +42,38 @@ export default function DashboardPage({ query }: { query?: { name: string, code:
         Promise.all([fetchGuilds(), fetchConnections()]);
     }, []);
 
+    const tabs = [
+        {
+            id: "connections",
+            label: l.dashboard.connections.title,
+            component: <ConnectionsComponent
+                connections={connections}
+                setConnections={setConnections}
+            />
+        },
+        {
+            id: "guilds",
+            label: l.dashboard.guilds.title,
+            component: <GuildsComponent
+                fetchGuilds={fetchGuilds}
+                guilds={guilds}
+            />
+        },
+        {
+            id: "teams",
+            label: l.dashboard.teams.title,
+            component: <TeamsComponent />
+        }
+    ]
+
     return (
         <ProtectedRoute loading={<ConnectionsProtectedSkeleton />}>
             <DefaultLayout className="mt-24">
-                <div className="flex w-full flex-col items-center tablet:items-start">
-                    <Tabs classNames={{
-                        cursor: "bg-neutral-700",
-                        tabList: "bg-neutral-800",
-                        base: "pl-3"
-                    }} aria-label="Options">
-                        <Tab
-                            className="flex items-start w-full"
-                            key="connections"
-                            title={l.dashboard.connections.title}
-                        >
-                            <ConnectionsComponent
-                                connections={connections}
-                                setConnections={setConnections}
-                            />
-                        </Tab>
-                        <Tab
-                            className="flex items-start w-full"
-                            key="guilds"
-                            title={l.dashboard.guilds.title}
-                        >
-                            <GuildsComponent
-                                fetchGuilds={fetchGuilds}
-                                guilds={guilds}
-                            />
-                        </Tab>
-                        <Tab
-                            className="flex items-start w-full"
-                            key="teams"
-                            title={l.dashboard.teams.title}
-                        >
-                            <TeamsComponent/>
-                        </Tab>
-                    </Tabs>
+                <div className="flex flex-col gap-6 w-full">
+                    <div className="tablet:ml-3">
+                        <DefaultTabs activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs} />
+                    </div>
+                    {tabs.find((t) => t.id === activeTab)?.component}
                 </div>
             </DefaultLayout>
             {(query && guilds) && (
