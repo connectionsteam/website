6f819aac699adf8db424d4b01077641ca8b6c943
Spyei <caio.salamao1234@gmail.com>
2024-08-21 21:24:54 -0300

    chore: pink theme for default button

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -9,19 +9,21 @@ interface Props {
     disabled?: boolean;
     href?: string;
     divclass?: string;
+    pink?: boolean;
+    notarget?: boolean;
 }
 
-export default function DefaultButton({ children, className, onClick, disabled, href, divclass }: Props) {
+export default function DefaultButton({ children, className, onClick, disabled, href, divclass, pink, notarget }: Props) {
     const sharedClasses = cn(
         "flex gap-2 h-full w-full items-center justify-center w-full rounded-lg bg-neutral-800 hover:bg-transparent transition disabled:hover:bg-neutral-800 text-center",
         className
     );
 
     return (
-        <div className={cn("p-[2px] bg-gradient-to-r from-fuchsia-500 to-indigo-500 rounded-lg w-full", divclass)}>
+        <div className={cn(`p-[2px] bg-gradient-to-r ${!pink ? "from-fuchsia-500 to-indigo-500" : "from-pink-600 to-rose-700"} rounded-lg w-full`, divclass)}>
             {href ? (
                 <Link
-                    target="_blank"
+                    target={notarget ? "_self" : "_blank"}
                     href={href}
                     className={sharedClasses}
                 >
